78e4e93751d1551227bb51b2026bd6a795c7e61c
Leonardo Leite <leonardofl87@gmail.com>
2011-12-09 21:41:11 -0200

    recuperando informacoes basicas do xml

--- consumer.py
+++ consumer.py
@@ -1,19 +1,21 @@
+#!/usr/bin/python3.2
+# -*- coding: utf-8 -*-
+#
 # Pequeno script que baixa a votação do código florestal
 # Fonte: http://rest.elkstein.org/2008/02/using-rest-in-python.html
-import urllib2
+import urllib.request
+from model import Proposicao
+
 
 tipo = 'pl'
 num = '1876'
 ano = '1999'
-url = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterVotacaoProposicao?tipo='+tipo+'&numero='+num+'&ano='+ano
-response = urllib2.urlopen(url).read()
+url = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterVotacaoProposicao?tipo=%s&numero=%s&ano=%s' % (tipo, num, ano)
+xml = urllib.request.urlopen(url).read()
+xml = str(xml, "utf-8")
 
-print response
+prop = Proposicao()
+prop.fromxml(xml)
+print(prop)
 
-# Para fazer POST
-#url = 'http:www;blbalab'
-#params = urllib.urlencode({
-#  'firstName':'John',
-#  'lastName':'Doe'
-#})
-#response = urllib2.urlopen(url, params).read()
+
--- model.py
+++ model.py
@@ -1,19 +1,35 @@
+#!/usr/bin/python3.2
+# -*- coding: utf-8 -*-
+#
 # Classes do modelo de negócio, no caso o XML da proposição
+import xml.etree.ElementTree as etree
+import io
 
 class Proposicao:
-  sigla
-  numero
-  ano
-  votacoes
+  sigla = ''
+  numero = ''
+  ano = ''
+  votacoes = []
+  
+  def fromxml(self, xml):
+    tree = etree.parse(io.StringIO(xml))
+    self.sigla = tree.find('Sigla').text
+    self.numero = tree.find('Numero').text
+    self.ano = tree.find('Ano').text
 
-class Votacao:
-  resumo
-  data
-  hora
-  deputados
+  def __str__(self):
+    return "%s %s/%s" % (self.sigla, self.numero, self.ano)
 
-class Deputado
-  nome
-  partifo
-  uf
-  voto
+#class Votacao:
+#  resumo
+#  data
+#  hora
+#  deputados
+
+#class Deputado:
+#  nome
+#  partifo
+#  uf
+#  voto
+  
+
