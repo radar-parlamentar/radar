f2b0e7f058ec8db10f522001626b8f6f8d405048
Saulo Trento <saulotrento@gmail.com>
2012-04-12 16:57:32 -0300

    Corrigido um bug em semelhanca_pca.py

--- semelhanca_pca.py
+++ semelhanca_pca.py
@@ -22,10 +22,10 @@ baseado nas proposições votadas em 2011
 utilizando PCA (análise de componentes primários)
 """
 
-import partidos2
+import partidos
 import sys
 
-partidos = []
+partidos_list = []
 vetores = []
 
 # recuperação dos vetores de votações
@@ -34,14 +34,14 @@ vfile = open(name, 'r')
 flag = 1
 for line in vfile:
   if flag % 2 == 1:
-    partidos.append(line.rstrip())
+    partidos_list.append(line.rstrip())
   else:
     vetores.append(eval(line))
   flag += 1
 
 # análise das semelhanças
 print('Análise PCA')
-p = partidos2.semelhanca_pca(vetores)
+p = partidos.semelhanca_pca(vetores)
 pc = p.pc()
 
 # impressão
@@ -50,5 +50,5 @@ for i in range(0, 4):
   print "%f " % ( p.eigen[i] / p.eigen.sum() )
 
 print "\nCoordenadas:"
-for i in range(0,len(partidos)):
-  print "%s: [%f, %f]" % (partidos[i], pc[i][0], pc[i][1])
+for i in range(0,len(partidos_list)):
+  print "%s: [%f, %f]" % (partidos_list[i], pc[i][0], pc[i][1])
