a70e17f0d3c579e3d16a918833d5db2af3b88433
Leonardo Leite <leonardofl87@gmail.com>
2012-05-14 17:31:27 -0300

    cria_bd refatorado para suportar diversas fontes de dados

--- cria_bd.py
+++ cria_bd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2012, Leonardo Leite, Saulo Trento
+# Copyright (C) 2012, Saulo Trento, Leonardo Leite
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Script cria_bd -- Cria um banco de dados sqlite3 em 'resultados/camara.db' a partir de requisições ao webservice da câmara pelas proposições elencadas no arquivo 'resultados/ids_que_existem.txt' (este último pode ser criado com o módulo ids_que_existem)
+"""módulo cria_bd 
+
+Classes:
+GeradorBD -- Cria um banco de dados sqlite3 a partir de proposições fornecidas (lista de objetos do tipo Proposicao) 
+
+Funções:
+cria_bd_camara_deputados -- Cria o banco de dados da câmara dos deputados em 'resultados/camara.db' a partir de requisições ao webservice da câmara pelas proposições elencadas no arquivo 'resultados/ids_que_existem.txt' (este último pode ser criado com o módulo ids_que_existem)
+
+cira_bc_cmsp -- Cria o banco de dados da Câmara Municipal do Município de São Paulo a partir dos XMLs em resultados/cmsp[ano].xml
 """
 
 import proposicoes
@@ -29,60 +37,66 @@ import model
 import ids_que_existem
 import sqlite3 as lite
 
-def arrumar_datas(arquivo_db):
-    con = lite.connect(arquivo_db)
-    with con:
-        #cur = con.cursor()
-        datas = con.execute("select idProp,idVot,data from VOTACOES;").fetchall()
-        for v in datas:
-            r = re.search('(\d*)/(\d*)/(\d*)',v[2])
-            formato_sql = r.group(3).zfill(4) + '-' + r.group(2).zfill(2) + '-' + r.group(1).zfill(2)
-            con.execute("update VOTACOES set data=? where idProp=?",(formato_sql,v[0]))
-    con.close()
-    return
-        
+class GeradorBD:
+    """Cria um banco de dados sqlite3 a partir de proposições fornecidas (lista de objetos do tipo Proposicao)"""
 
-if __name__ == "__main__":
+    def __init__(self, proposicoes = [], db = "resultados/votos.db"):
+        """ Argumentos:
+        proposicoes - lista de objetos do tipo Proposicao
+        db - string com a localização do banco de dados a ser gerado (default = "resultados/votos.db")
+        """
+        self.proposicoes = proposicoes
+        self.db = db
 
-    os.system('rm resultados/camara.db.backup') # Apagar backup anterior.
-    os.system('mv resultados/camara.db resultados/camara.db.backup') # Faz backup do bd antigo.
-    os.system('rm resultados/camara.db') # Apagar bd antigo para fazer um novo em folha.
+    def arrumar_datas(self):
+        con = lite.connect(self.db)
+        with con:
+            #cur = con.cursor()
+            datas = con.execute("select idProp,idVot,data from VOTACOES;").fetchall()
+            for v in datas:
+                r = re.search('(\d*)/(\d*)/(\d*)',v[2])
+                formato_sql = r.group(3).zfill(4) + '-' + r.group(2).zfill(2) + '-' + r.group(1).zfill(2)
+                con.execute("update VOTACOES set data=? where idProp=?",(formato_sql,v[0]))
+        con.close()
+        return
+            
+    def _prepara_backup(self):
+        os.system('rm %s.backup') % self.db # Apagar backup anterior.
+        os.system('mv %s %s.backup') % self.db # Faz backup do bd antigo.
+        os.system('rm %s') % self.db # Apagar bd antigo para fazer um novo em folha.
+        print 'resultados/camara.db renomeado para resultados/camara.db.backup'
+        print 'Backup anterior, se havia, foi apagado.'
 
-    print 'resultados/camara.db renomeado para resultados/camara.db.backup'
-    print 'Backup anterior, se havia, foi apagado.'
-    print 'Entre parenteses, (id da proposicao,numero de votacoes).'
-    print 'Proposicoes sem votacoes aparecem como um ponto.'
+    def gera_bd(self):
 
-    proposicoes = [] # lista de objetos da classe model.Proposicao.
-    numero_votacoes = [] # lista de ints que informa quantas votacoes cada proposicao da lista acima tem.
+        self._prepara_backup()
 
-    lista_proposicoes = ids_que_existem.parse_txt('resultados/ids_que_existem.txt')
-    con = lite.connect('resultados/camara.db') # abrir conexão com bd.
-    with con:
-        cur = con.cursor()
-        # Para criar tabela só se já não existir: create table if not exists TableName
-        # Para testar se tabela existe: SELECT name FROM sqlite_master WHERE type='table' AND name='table_name';
-        cur.execute("CREATE TABLE if not exists  PROPOSICOES(idProp INT, tipo TEXT, num TEXT, ano TEXT, ementa TEXT, explicacao TEXT, situacao TEXT, num_votacoes INT)")
-        cur.execute("CREATE TABLE if not exists DEPUTADOS(idDep INT, deputado TEXT, partido TEXT, uf TEXT)")
-        cur.execute("CREATE TABLE if not exists VOTACOES(idProp INT, idVot INT, resumo TEXT, data TEXT, hora TEXT, sim TEXT, nao TEXT, abstencao TEXT, obstrucao TEXT)")
-        cur.execute("CREATE TABLE if not exists PARTIDOS(numero INT, nome TEXT)")
-    con.close()
+        print 'Entre parenteses, (id da proposicao,numero de votacoes).'
+        print 'Proposicoes sem votacoes aparecem como um ponto.'
 
-    for iprop in lista_proposicoes:
-        #print 'AVALIANDO %s %s %s' % (iprop['tipo'],iprop['num'],iprop['ano'])
-        p = camaraws.obter_votacao(iprop['tipo'], iprop['num'],iprop['ano']) # Obtém proposição e suas votações do web service
-        if p != None:
-            proposicoes.append(p)
-            numero_votacoes.append(len(p.votacoes))
-            #print 'Houveram votacoes. Votacoes encontradas: %d' % (len(p.votacoes))
+        numero_votacoes = [] # lista de ints que informa quantas votacoes cada proposicao da lista acima tem.
+
+        con = lite.connect(self.db) # abrir conexão com bd.
+        with con:
+            cur = con.cursor()
+            # Para criar tabela só se já não existir: create table if not exists TableName
+            # Para testar se tabela existe: SELECT name FROM sqlite_master WHERE type='table' AND name='table_name';
+            cur.execute("CREATE TABLE if not exists  PROPOSICOES(idProp INT, tipo TEXT, num TEXT, ano TEXT, ementa TEXT, explicacao TEXT, situacao TEXT, num_votacoes INT)")
+            cur.execute("CREATE TABLE if not exists DEPUTADOS(idDep INT, deputado TEXT, partido TEXT, uf TEXT)")
+            cur.execute("CREATE TABLE if not exists VOTACOES(idProp INT, idVot INT, resumo TEXT, data TEXT, hora TEXT, sim TEXT, nao TEXT, abstencao TEXT, obstrucao TEXT)")
+            cur.execute("CREATE TABLE if not exists PARTIDOS(numero INT, nome TEXT)")
+        con.close()
+
+        for prop in self.proposicoes:
+            numero_votacoes.append(len(prop.votacoes))
             sys.stdout.write('(%s,%d),'%(iprop['id'],len(p.votacoes)))
             sys.stdout.flush()
             #adicionar proposicao na tabela PROPOSICOES
             con = lite.connect('resultados/camara.db')
-            con.execute("INSERT INTO PROPOSICOES VALUES(?,?,?,?,?,?,?,?)",(p.id,p.sigla,p.numero,p.ano,p.ementa,p.explicacao,p.situacao,len(p.votacoes)))
+            con.execute("INSERT INTO PROPOSICOES VALUES(?,?,?,?,?,?,?,?)",(prop.id, prop.sigla, prop.numero, prop.ano, prop.ementa, prop.explicacao, prop.situacao, len(prop.votacoes)))
             con.commit()
             con.close()
-            for v in p.votacoes:
+            for v in prop.votacoes:
                 sim = []
                 nao = []
                 abstencao = []
@@ -97,8 +111,8 @@ if __name__ == "__main__":
                         abstencao.append(idDep)
                     if d.voto[0] == 'O':
                         obstrucao.append(idDep)
-                pid = p.id
-                votid = p.votacoes.index(v) + 1
+                pid = prop.id
+                votid = prop.votacoes.index(v) + 1
                 resum = v.resumo
                 data = v.data
                 hora = v.hora
@@ -106,12 +120,31 @@ if __name__ == "__main__":
                 snao = str(nao)
                 sabs = str(abstencao)
                 sobs = str(obstrucao)
-                con = lite.connect('resultados/camara.db')
+                con = lite.connect(self.db)
                 con.execute("INSERT INTO VOTACOES VALUES(?,?,?,?,?,?,?,?,?)",(pid, votid, resum, data, hora, ssim, snao, sabs, sobs))
                 con.commit()
                 con.close()
-        else:
-            sys.stdout.write('.')
-            sys.stdout.flush()
 
-    arrumar_datas('resultados/camara.db')
+        self.arrumar_datas()
+
+def cria_bd_camara_deputados():
+    """Cria o banco de dados da câmara dos deputados em 'resultados/camara.db' a partir de requisições ao webservice da câmara pelas proposições elencadas no arquivo 'resultados/ids_que_existem.txt' (este último pode ser criado com o módulo ids_que_existem)"""
+
+    props = []
+    lista_proposicoes = ids_que_existem.parse_txt('resultados/ids_que_existem.txt')
+    for iprop in lista_proposicoes:
+        print 'AVALIANDO %s %s %s' % (iprop['tipo'],iprop['num'],iprop['ano'])
+        p = camaraws.obter_votacao(iprop['tipo'], iprop['num'],iprop['ano']) # Obtém proposição e suas votações do web service
+        if p != None:
+            props.append(p)
+
+    print 'Agora sim gerando o banco'
+    gerador = GeradorBD(props, 'resultados/camara.db')
+    gerador.gera_bd()
+
+def cria_bd_cmsp():
+    print "TODO..."
+
+if __name__ == "__main__":
+    cria_bd_camara_deputados()    
+
