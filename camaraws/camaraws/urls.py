d4c8115892dbf9e9c5e6bdb78906317a78e7a082
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2012-05-21 11:33:09 -0300

    Atualizando algumas coisas para versão 1.4 do django

--- camaraws/camaraws/urls.py
+++ camaraws/camaraws/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls.defaults import patterns, include, url
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
--- camaraws/models.py
+++
@@ -1,3 +0,0 @@
-# coding=utf8
-from django.db import models
-from django.core.exceptions import ValidationError
