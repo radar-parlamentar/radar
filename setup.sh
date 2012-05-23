7aff4b342270668bd9a969661e9485ffe89d1c7f
Leonardo Leite <leonardofl87@gmail.com>
2012-05-22 23:29:01 -0300

    ajuda pra quem for executar o código

---
+++ setup.sh
@@ -0,0 +1,8 @@
+Instalação das dependências no Ubuntu
+
+sudo apt-get install python-numpy python-matplotlib python-pip sqlite3
+sudo pip install svgwrite
+wget "http://www.djangoproject.com/download/1.4/tarball/" -O Django-1.4.tar.gz
+tar xzvf Django-1.4.tar.gz
+cd Django-1.4
+sudo python setup.py install 
