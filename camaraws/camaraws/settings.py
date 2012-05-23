86fd2c1bd6d066a2807953864d07706b9c75cf15
Leonardo Leite <leonardofl87@gmail.com>
2012-05-22 22:04:33 -0300

    Django já cria BD a partir de modelagem.models

--- camaraws/camaraws/settings.py
+++ camaraws/camaraws/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'votacoes',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/leonardo/votacoes.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -27,11 +27,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'modelagem',
 )
 
 # A sample logging configuration. The only tangible logging
--- camaraws/modelagem/models.py
+++ camaraws/modelagem/models.py
@@ -17,7 +17,6 @@
 
 from __future__ import unicode_literals
 from django.db import models
-from django.core.exceptions import ValidationError
 
 OPCOES = (
     ('SIM', 'Sim'),
@@ -38,7 +37,8 @@ ESFERAS = (
     ('FEDERAL', 'Federal'),
 )
 
-class Partido(django.db.models.Model):
+
+class Partido(models.Model):
     """Partido político.
 
     Atributos:
@@ -56,7 +56,111 @@ class Partido(django.db.models.Model):
         return unicode(self).encode('utf-8')
 
 
-class Proposicao(django.db.models.Model):
+class HistoricoTamanho(models.Model):
+
+    tamanho = models.IntegerField()
+    inicio = models.DateField(blank=True)
+    fim = models.DateField(blank=True)
+
+
+class Composicao(models.Model):
+    """Participação de um partido na casa"""
+
+    partido = models.ForeignKey(Partido)
+    historico = models.ManyToManyField(HistoricoTamanho)
+
+
+class CasaLegislativa(models.Model):
+    """Instituição tipo Senado, Câmara etc
+
+    Atributos:
+        nome -- string; ex 'Câmara Municipal de São Paulo'
+        esfera -- string (municipal, estadual, federal)
+        local -- string; ex 'São Paulo' para a CMSP
+        tamanhos -- lista de objetos do tipo HistoricoTamanho representando quantas cadeiras possui a casa
+        composicao -- lista de objetos do tipo Composicao
+    """
+
+    nome = models.CharField(max_length=100)
+    esfera = models.CharField(max_length=10, choices=ESFERAS)
+    local = models.CharField(max_length=100)
+    tamanhos = models.ManyToManyField(HistoricoTamanho, blank=True)
+    composicoes = models.ManyToManyField(Composicao, blank=True)
+
+    def __unicode__(self):
+        return self.nome
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+class Legislatura(models.Model):
+    """O mandato exercido por um parlamentar.
+
+    Atributos:
+        localidade -- string; ex 'SP', 'RJ' se for no senado ou câmara dos deputados
+        casa_legislativa -- objeto do tipo CasaLegislativa
+        partido -- objeto do tipo Partido
+        inicio, fim -- datas indicando o período
+    """
+
+    localidade = models.CharField(max_length=100, blank=True)
+    casa_legislativa = models.ForeignKey(CasaLegislativa, blank=True)
+    partido = models.ForeignKey(Partido)
+    inicio = models.DateField(blank=True)
+    fim = models.DateField(blank=True)
+
+    def __unicode__(self):
+        return "%s" % self.periodo
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+class Parlamentar(models.Model):
+    """Um parlamentar.
+
+    Atributos:
+        id_parlamentar - string identificadora de acordo a fonte de dados
+        nome, genero -- strings
+        legislatura -- lista de objetos do tipo Legislatura
+
+    Métodos:
+        partido_atual()
+        legislatura_atual()
+    """
+
+    id_parlamentar = models.CharField(max_length=100) # obs: não é chave primária! 
+    nome = models.CharField(max_length=100)
+    genero = models.CharField(max_length=10, choices=GENEROS, blank=True)
+    legislaturas = models.ManyToManyField(Legislatura)
+
+    def partido_atual(self):
+        """Retorna partido atual do parlamentar.
+
+        Retorno: objeto do tipo Partido
+        """
+        return self.legislatura_atual().partido
+
+    def legislatura_atual(self):
+        """Retorna legislatura atual do parlamentar.
+
+        Retorno: objeto do tipo Legislatura
+        """
+        return self.legislaturas[-1] 
+
+    def __unicode__(self):
+        leg = self.legislatura_atual()
+        if (leg.localidade):
+            return "%s (%s-%s)" % (self.nome, leg.partido, leg.localidade)
+        else:
+            return "%s (%s)" % (self.nome, leg.partido)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+class Proposicao(models.Model):
     """Proposição parlamentar (proposta de lei).
     
     Atributos:
@@ -96,7 +200,25 @@ class Proposicao(django.db.models.Model):
         return unicode(self).encode('utf-8')
 
 
-class Votacao(django.db.models.Model):
+class Voto(models.Model):
+    """Um voto dado por um parlamentar em uma votação.
+
+    Atributos:
+        parlamentar -- objeto do tipo Parlamentar
+        opcao -- qual foi o voto do parlamentar (sim, não, abstenção, obstrução, não votou)
+    """
+
+    parlamentar = models.ForeignKey(Parlamentar)
+    opcao = models.CharField(max_length=10, choices=OPCOES)
+
+    def __unicode__(self):
+        return "%s votou %s" % (self.parlamentar, self.opcao)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+class Votacao(models.Model):
     """Votação em planário.
     
     Atributos:
@@ -143,126 +265,6 @@ class Votacao(django.db.models.Model):
 
     def __str__(self):
         return unicode(self).encode('utf-8')    
-
-
-class Voto(django.db.models.Model):
-    """Um voto dado por um parlamentar em uma votação.
-
-    Atributos:
-        parlamentar -- objeto do tipo Parlamentar
-        opcao -- qual foi o voto do parlamentar (sim, não, abstenção, obstrução, não votou)
-    """
-
-    parlamentar = models.ForeignKey(Parlamentar)
-    opcao = models.CharField(max_length=10, choices=OPCOES)
-
-    def __unicode__(self):
-        return "%s votou %s" % (self.parlamentar, self.opcao)
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-class Parlamentar(django.db.models.Model):
-    """Um parlamentar.
-
-    Atributos:
-        id_parlamentar - string identificadora de acordo a fonte de dados
-        nome, genero -- strings
-        legislatura -- lista de objetos do tipo Legislatura
-
-    Métodos:
-        partido_atual()
-        legislatura_atual()
-    """
-
-    id_parlamentar = models.CharField(max_length=100) # obs: não é chave primária! 
-    nome = models.CharField(max_length=100)
-    genero = models.CharField(max_length=10, choices=GENEROS, blank=True)
-    legislaturas = models.ManyToManyField(Legislatura)
-
-    def partido_atual(self):
-        """Retorna partido atual do parlamentar.
-
-        Retorno: objeto do tipo Partido
-        """
-        return self.legislatura_atual().partido
-
-    def legislatura_atual(self):
-        """Retorna legislatura atual do parlamentar.
-
-        Retorno: objeto do tipo Legislatura
-        """
-        return self.legislaturas[-1] 
-
-    def __unicode__(self):
-        leg = self.legislatura_atual()
-        if (leg.localidade):
-            return "%s (%s-%s)" % (self.nome, leg.partido, leg.localidade)
-        else:
-            return "%s (%s)" % (self.nome, leg.partido)
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-
-class Legislatura(django.db.models.Model):
-    """O mandato exercido por um parlamentar.
-
-    Atributos:
-        localidade -- string; ex 'SP', 'RJ' se for no senado ou câmara dos deputados
-        casa_legislativa -- objeto do tipo CasaLegislativa
-        partido -- objeto do tipo Partido
-        inicio, fim -- datas indicando o período
-    """
-
-    localidade = models.CharField(max_length=100, blank=True)
-    casa_legislativa = models.ForeignKey(CasaLegislativa, blank=True)
-    partido = models.ForeignKey(Partido)
-    inicio = models.DateField(blank-True)
-    fim = models.DateField(blank=True)
-
-    def __unicode__(self):
-        return "%s" % self.periodo
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-class CasaLegislativa(django.db.models.Model):
-    """Instituição tipo Senado, Câmara etc
-
-    Atributos:
-        nome -- string; ex 'Câmara Municipal de São Paulo'
-        esfera -- string (municipal, estadual, federal)
-        local -- string; ex 'São Paulo' para a CMSP
-        tamanhos -- lista de objetos do tipo HistoricoTamanho representando quantas cadeiras possui a casa
-        composicao -- lista de objetos do tipo Composicao
-    """
-
-    nome = models.CharField(max_length=100)
-    esfera = models.CharField(max_length=10, choices=ESFERAS)
-    local = models.CharField(max_length=100)
-    tamanhos = models.ManyToManyField(HistoricoTamanho, blank=True)
-    composicoes = models.ManyToManyField(Composicao, blank=True)
-
-    def __unicode__(self):
-        return self.nome
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-
-class Composicao(django.db.models.Model):
-    """Participação de um partido na casa"""
-
-    partido = models.ForeignKey(Partido)
-    historico = models.ManyToManyField(HistoricoTamanho)
-
-
-class HistoricoTamanho(django.db.models.Model):
-
-    tamanho = models.IntegerField()
-    inicio = models.DateField(blank=True)
-    fim = models.DateField(blank=True)
 
 
 class VotosAgregados:
