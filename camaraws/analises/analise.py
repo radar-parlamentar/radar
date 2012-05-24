92b0849f6c2de040d7c82902873560670fd81c5f
Leonardo Leite <leonardofl87@gmail.com>
2012-05-24 01:01:09 -0300

    pca da cmsp para todo o período [2010,2012]

--- camaraws/analises/analise.py
+++ camaraws/analises/analise.py
@@ -17,12 +17,13 @@
 
 """Módulo analise"""
 
-import re
 import numpy
 import pca
-import sys
 from django.utils.dateparse import parse_datetime
 from modelagem import models
+from matplotlib.pyplot import figure, show, scatter,text
+from matplotlib.patches import Ellipse
+import matplotlib.colors
 
 
 class Analise:
@@ -33,15 +34,18 @@ class Analise:
         self.votacoes = models.Votacao.objects.all() 
         self.partidos = models.Partido.objects.all()
         self.num_votacoes = len(self.votacoes)
+        self.vetores_votacao = []
+        self.pca_partido = []
 
     def _inicializa_vetores(self):
-        """ Cria os 'vetores de votação' para cada partido. 
-            O 'vetor' usa um número entre -1 (não) e 1 (sim) para representar a "posição média" do partido em cada votação, 
-            tendo N dimensões correspondentes às N votações.
-            Aproveita para calcular o tamanho dos partidos, presença dos parlamentares, etc.
+        """Cria os 'vetores de votação' para cada partido. 
 
-            Retorna a 'matriz de votações', em que cada linha é um vetor de votações de um partido 
-            A ordenação das linhas segue a ordem de self.partidos
+        O 'vetor' usa um número entre -1 (não) e 1 (sim) para representar a "posição média" do partido em cada votação, 
+        tendo N dimensões correspondentes às N votações.
+        Aproveita para calcular o tamanho dos partidos, presença dos parlamentares, etc.
+
+        Retorna a 'matriz de votações', em que cada linha é um vetor de votações de um partido 
+                A ordenação das linhas segue a ordem de self.partidos
         """
 
         # numpy.zeros((n,m)) gera matriz
@@ -65,10 +69,83 @@ class Analise:
         return self.vetores_votacao
 
 
-#    def _pca_partido(self):
+    def _pca_partido(self):
+        """Roda a análise de componentes principais por partidos.
 
-#    def partidos_2d(self):
-#        """Retorna matriz com as coordenadas dos partidos no plano 2d formado pelas duas primeiras componentes principais."""
+        Guarda o resultado em self.pca
+        Retorna um dicionário no qual as chaves são as siglas dos partidos
+        e o valor de cada chave é um vetor com as n dimensões da análise pca
+        """
+        if not self.pca_partido:
+            if not self.vetores_votacao:
+                self._inicializa_vetores()
+            matriz = self.vetores_votacao - self.vetores_votacao.mean(axis=0)
+            self.pca_partido = pca.PCA(matriz)
+
+        dicionario = {}
+        for partido, vetor in zip(self.partidos, self.pca_partido.U):
+            dicionario[partido.nome] = vetor
+        return dicionario
 
 
+    def partidos_2d(self):
+        """Retorna mapa com as coordenadas dos partidos no plano 2D formado pelas duas primeiras componentes principais.
+
+        A chave do mapa é o nome do partido (string) e o valor é uma lista de duas posições [x,y].
+        """
+
+        coordenadas = self._pca_partido()
+        for partido in coordenadas.keys():
+            coordenadas[partido] = (coordenadas[partido])[0:2]
+        return coordenadas
+
+
+    def figura(self, escala=10):
+        """Apresenta um plot de bolhas (usando matplotlib) com os partidos de tamanho maior ou igual a tamanho_min com o primeiro componente principal no eixo x e o segundo no eixo y.
+        """
+        dados = self.partidos_2d()
+
+        fig = figure(1)
+        fig.clf()
+
+        cores_partidos = {'PT'   :'#FF0000',
+                      'PSOL' :'#FFFF00',
+                      'PV'   :'#00CC00',
+                      'DEM'  :'#002664',
+                      'PSDB' :'#0059AB',
+                      'PSD'  :'#80c341',
+                      'PMDB' :'#CC0000',
+                      'PR'   :'#110274',
+                      'PSC'  :'#25b84a',
+                      'PSB'  :'#ff8d00',
+                      'PP'   :'#203487',
+                      'PCdoB':'#da251c',
+                      'PTB'  :'#1f1a17',
+                      'PPS'  :'#fea801',
+                      'PDT'  :'#6c85b1',
+                      'PRB'  :'#67a91e'}
+
+        lista_cores_partidos = []
+        for partido in self.partidos:
+            if partido.nome in cores_partidos:
+                lista_cores_partidos.append(cores_partidos[partido.nome])
+            else:
+                lista_cores_partidos.append((1,1,1))
+
+        colormap_partidos = matplotlib.colors.ListedColormap(lista_cores_partidos,name='partidos')
+
+        ax = fig.add_subplot(111, autoscale_on=True) #, xlim=(-1,5), ylim=(-5,3))
+        x = []
+        y = []
+        for partido in self.partidos:
+            x.append(dados[partido.nome][0])
+            y.append(dados[partido.nome][1])
+        t = 20 # self.tamanho_partido
+        size = numpy.array(t) * escala
+        scatter(x, y, size, range(len(x)), marker='o', cmap=colormap_partidos) #, norm=None, vmin=None, vmax=None, alpha=None, linewidths=None, faceted=True, verts=None, hold=None, **kwargs)
+
+        for partido in self.partidos:
+            text(dados[partido.nome][0]+.005,dados[partido.nome][1],partido.numero,fontsize=12,stretch=100,alpha=1)
+
+        show()
 
