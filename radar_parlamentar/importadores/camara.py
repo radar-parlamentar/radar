5439379e2f11b968298fb5a2f651248ddbd64ca0
Leonardo Leite <leonardofl87@gmail.com>
2012-12-15 21:16:12 -0200

    mudando leitura de arquivo em parse_ids_existem tb

--- radar_parlamentar/importadores/camara.py
+++ radar_parlamentar/importadores/camara.py
@@ -102,8 +102,9 @@ class ProposicoesFinder:
         # ex: "485262: MPV 501/2010"
         regexp = '^([0-9]*?): ([A-Z]*?) ([0-9]*?)/([0-9]{4})'
         proposicoes = []
-        with open(file_name, 'r') as prop_file:
-            res = re.search(regexp, prop_file.readline())
+        f = open(file_name, 'r')
+        for line in f:
+            res = re.search(regexp, line)
             if res:
                 proposicoes.append({'id':res.group(1), 'sigla':res.group(2), 'num':res.group(3), 'ano':res.group(4)})
         return proposicoes
