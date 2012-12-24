670e5a703688b6f3f7ceed193c032203f3b71329
leonardofl <leonardofl87@gmail.com>
2012-12-24 12:33:00 -0200

    troca de licença para affero; copyright para Hideo em modelagem

--- radar_parlamentar/analises/tests.py
+++ radar_parlamentar/analises/tests.py
@@ -2,18 +2,20 @@
 
 # Copyright (C) 2012, Leonardo Leite
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# This file is part of Radar Parlamentar.
+# 
+# Radar Parlamentar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# Radar Parlamentar is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# along with Radar Parlamentar.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 from django.test import TestCase
