d4b35188e33b8e33d4ae3cfa6c43a74b04ea6af4
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2013-01-15 01:53:39 -0200

    Alterações realizadas no radar_parlamentar.js  - Mudança na organização do arquivo, transformando-o num objeto e exportando apenas a função de inicialização;  - Cálculo automático do maior e menor período para poder interpolar;  - Recupera automáticamente do json a label do primeiro período;  - Agora a animação de mouseover do período para se movimentar ao longo do tempo é feita sobre todo o nome da label do período;  - Foi mantida a linha de código comentada da função que carrega os dados via hardcoded para testes;  - Foi feito o ajuste do gráfico para ele caber ao lado da legenda de partidos.

--- radar_parlamentar/static/files/codes/js/radar_parlamentar_plot.js
+++ radar_parlamentar/static/files/codes/js/radar_parlamentar_plot.js
@@ -1,197 +1,220 @@
-// Various accessors that specify the four dimensions of data to visualize.
-function x(d) { return d.x; } // income (per capta) from original json
-function y(d) { return d.y; } // life expectancy from original json
-function tamanho(d) { return d.tamanho; } // population from original json
-function cor(d) { return d.cor; } // based on region from original json
-function nome(d) { return d.nome; } // name from original json
-function numero(d) { return d.numero; } // new parameter to json
-
-var periodo_min = 1,
-    periodo_max = 2;
-
-// Chart dimensions.
-var margin = {top: 19.5, right: 100, bottom: 19.5, left: 39.5},
-    width = 880 - margin.right,
-    height = 590 - margin.top - margin.bottom;
-
-// Various scales. These domains make assumptions of data, naturally.
-var xScale = d3.scale.linear().domain([0, 100]).range([0, width]),
-    yScale = d3.scale.linear().domain([0, 100]).range([height, 0]),
-    radiusScale = d3.scale.sqrt().domain([0, 3]).range([0, 40]),
-    colorScale = d3.scale.category10();
-
-// The x & y axes.
-var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-// Create the SVG container and set the origin.
-var svg = d3.select("#animacao").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//*
-// Add the x-axis.
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-// Add the y-axis.
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
-//*/
-
-//*
-// Add an x-axis label.
-svg.append("text")
-    .attr("class", "x label")
-    .attr("text-anchor", "end")
-    .attr("x", width)
-    .attr("y", height - 6)
-    .text("income per capita, inflation-adjusted (dollars)");
-
-// Add a y-axis label.
-svg.append("text")
-    .attr("class", "y label")
-    .attr("text-anchor", "end")
-    .attr("y", 6)
-    .attr("dy", ".75em")
-    .attr("transform", "rotate(-90)")
-    .text("life expectancy (years)");
-//*/
-
-// Add the year label; the value is set on transition.
-var label = svg.append("text")
-    .attr("class", "year label")
-    .attr("text-anchor", "end")
-    .attr("y", height - 24)
-    .attr("x", width)
-    .text(periodo_min);
-
-function plot_casa(nome_curto_casa_legislativa) {
-
-  d3.json("/analises/analise/" + nome_curto_casa_legislativa + "/json_pca", plot_data)
-}
-
-function plot_data(dados) {
-
-  var partidos = dados.partidos,
-      periodos = dados.periodos;
-
-  // A bisector since many nation's data is sparsely-defined.
-  var bisect = d3.bisector(function(d) { return d[0]; });
-
-  // Add a dot per nation. Initialize the data at 1800, and set the colors.
-  var dot = svg.append("g")
-      .attr("class", "dots")
-    .selectAll(".dot")
-      .data(interpolateData(1))
-    .enter().append("circle")
-      .attr("class", "dot")
-      .style("fill", function(d) { return colorScale(cor(d)); })
-      .call(position)
-      .sort(order);
-
-  // Add a title.
-  dot.append("title")
-      .text(function(d) { return nome(d); });
-
-  // Add an overlay for the year label.
-  var box = label.node().getBBox();
-
-  var overlay = svg.append("rect")
-        .attr("class", "overlay")
-        .attr("x", box.x)
-        .attr("y", box.y)
-        .attr("width", box.width)
-        .attr("height", box.height)
-        .on("mouseover", enableInteraction);
-
-  // Start a transition that interpolates the data based on year.
-  svg.transition()
-      .duration(30000)
-      .ease("linear")
-      .tween("year", tweenYear)
-      .each("end", enableInteraction);
-
-  // Positions the dots based on data.
-  function position(dot) {
-    dot .attr("cx", function(d) { return xScale(x(d)); })
-        .attr("cy", function(d) { return yScale(y(d)); })
-        .attr("r", function(d) { return radiusScale(tamanho(d)); });
+Plot = (function ($) {
+  
+  function initialize(dataPath){
+    _load(dataPath);
   }
 
-  // Defines a sort order so that the smallest dots are drawn on top.
-  function order(a, b) {
-    return tamanho(b) - tamanho(a);
+  // Function to load the data and draw the chart
+  function _load(nome_curto_casa_legislativa) {
+    d3.json("/analises/analise/" + nome_curto_casa_legislativa + "/json_pca", _plot_data);
+    //para testes com arquivo hardcoded
+    //d3.json("/static/files/partidos.json", _plot_data);
   }
+  
+  function x(d) { return d.x; } // income (per capta) from original json
+  function y(d) { return d.y; } // life expectancy from original json
+  function tamanho(d) { return d.tamanho; } // population from original json
+  function cor(d) { return d.cor; } // based on region from original json
+  function nome(d) { return d.nome; } // name from original json
+  function numero(d) { return d.numero; } // new parameter to json
+  
+  // Chart dimensions.
+  var margin = {top: 19.5, right: 100, bottom: 19.5, left: 39.5},
+      width = 880 - margin.right - margin.left;
+      height = 590 - margin.top - margin.bottom;
+  
+  // Various scales. These domains make assumptions of data, naturally.
+  var xScale = d3.scale.linear().domain([0, 100]).range([0, width]),
+      yScale = d3.scale.linear().domain([0, 100]).range([height, 0]),
+      radiusScale = d3.scale.sqrt().domain([0, 9]).range([0, 40]),
+      colorScale = d3.scale.category10();
+  
+  // The x & y axes.
+  var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
+      yAxis = d3.svg.axis().scale(yScale).orient("left");
+    
+  // Function that draw the chart
+  function _plot_data(dados) {
+    // Various accessors that specify the four dimensions of data to visualize.
+    
+    var periodo_min = 1,
+        periodo_max = 2,
+        first_label = "1o sem. 1788";
+    
+    // Create the SVG container and set the origin.
+    var svg = d3.select("#animacao").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .style("position", "relative")
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    /*
+    // Add the x-axis.
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+    
+    // Add the y-axis.
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+    //*/
+    
+    /*
+    // Add an x-axis label.
+    svg.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height - 6)
+        .text("income per capita, inflation-adjusted (dollars)");
+    
+    // Add a y-axis label.
+    svg.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("y", 6)
+        .attr("dy", ".75em")
+        .attr("transform", "rotate(-90)")
+        .text("life expectancy (years)");
+    //*/
+    
+    var partidos = dados.partidos,
+        periodos = dados.periodos;
+    
+    //Carregando os períodos extremos dos dados
+    periodo_min = Number(d3.min(d3.keys(periodos)));
+    periodo_max = Number(d3.max(d3.keys(periodos)));
 
-  // After the transition finishes, you can mouseover to change the year.
-  function enableInteraction() {
-    var yearScale = d3.scale.linear()
-        .domain([periodo_min, periodo_max])
-        .range([box.x + 10, box.x + box.width - 10])
-        .clamp(true);
+    first_label = periodos[periodo_min];
 
-    // Cancel the current transition, if any.
-    svg.transition().duration(0);
 
-    overlay
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout)
-        .on("mousemove", mousemove)
-        .on("touchmove", mousemove);
+    // Add the year label; the value is set on transition.
+    var label = svg.append("text")
+        .attr("class", "year label")
+        .attr("text-anchor", "end")
+        .attr("y", '34')
+        .attr("x", width + margin.right)
+        .text(first_label);
+    
+    // A bisector since many nation's data is sparsely-defined.
+    var bisect = d3.bisector(function(d) { return d[0]; });
 
-    function mouseover() {
-      label.classed("active", true);
+    // Add a dot per nation. Initialize the data at 1800, and set the colors.
+    var dot = svg.append("g")
+        .attr("class", "dots")
+      .selectAll(".dot")
+        .data(interpolateData(1))
+      .enter().append("circle")
+        .attr("class", "dot")
+        .style("fill", function(d) { return colorScale(cor(d)); })
+        .call(position)
+        .sort(order);
+
+    // Add a title.
+    dot.append("title")
+        .text(function(d) { return nome(d); });
+
+    // Add an overlay for the year label.
+    var box = label.node().getBBox();
+
+    var overlay = svg.append("rect")
+          .attr("class", "overlay")
+          .attr("x", box.x)
+          .attr("y", box.y)
+          .attr("width", box.width)
+          .attr("height", box.height)
+          .on("mouseover", enableInteraction);
+    
+    // Start a transition that interpolates the data based on year.
+    svg.transition()
+        .duration(10000)
+        .ease("linear")
+        .tween("year", tweenYear)
+        .each("end", enableInteraction);
+  
+    // Positions the dots based on data.
+    function position(dot) {
+      dot .attr("cx", function(d) { return xScale(x(d)); })
+          .attr("cy", function(d) { return yScale(y(d)); })
+          .attr("r", function(d) { return radiusScale(tamanho(d)); });
     }
 
-    function mouseout() {
-      label.classed("active", false);
+    // Defines a sort order so that the smallest dots are drawn on top.
+    function order(a, b) {
+      return tamanho(b) - tamanho(a);
     }
 
-    function mousemove() {
-      displayYear(yearScale.invert(d3.mouse(this)[0]));
+    // After the transition finishes, you can mouseover to change the year.
+    function enableInteraction() {
+      var yearScale = d3.scale.linear()
+          .domain([periodo_min, periodo_max])
+          .range([box.x + 10, box.x + box.width - 10])
+          .clamp(true);
+
+      // Cancel the current transition, if any.
+      svg.transition().duration(0);
+
+      overlay
+          .on("mouseover", mouseover)
+          .on("mouseout", mouseout)
+          .on("mousemove", mousemove)
+          .on("touchmove", mousemove);
+
+      function mouseover() {
+        label.classed("active", true);
+      }
+
+      function mouseout() {
+        label.classed("active", false);
+      }
+
+      function mousemove() {
+        displayYear(yearScale.invert(d3.mouse(this)[0]));
+      }
+    }
+
+    // Tweens the entire chart by first tweening the year, and then the data.
+    // For the interpolated data, the dots and label are redrawn.
+    function tweenYear() {
+      var year = d3.interpolateNumber(periodo_min, periodo_max);
+      return function(t) { displayYear(year(t)); };
+    }
+
+    // Updates the display to show the specified year.
+    function displayYear(year) {
+      dot.data(interpolateData(year), nome).call(position).sort(order);
+      label.text(periodos[Math.round(year)]);
+    }
+
+    // Interpolates the dataset for the given (fractional) year.
+    function interpolateData(year) {
+      return partidos.map(function(d) {
+        return {
+          nome: d.nome,
+          cor: d.cor,
+          tamanho: interpolateValues(d.tamanho, year),
+          x: interpolateValues(d.x, year),
+          y: interpolateValues(d.y, year)
+        };
+      });
+    }
+
+    // Finds (and possibly interpolates) the value for the specified year.
+    function interpolateValues(values, year) {
+      var i = bisect.left(values, year, 0, values.length - 1),
+          a = values[i];
+      if (i > 0) {
+        var b = values[i - 1],
+            t = (year - a[0]) / (b[0] - a[0]);
+        return a[1] * (1 - t) + b[1] * t;
+      }
+      return a[1];
     }
   }
 
-  // Tweens the entire chart by first tweening the year, and then the data.
-  // For the interpolated data, the dots and label are redrawn.
-  function tweenYear() {
-    var year = d3.interpolateNumber(periodo_min, periodo_max);
-    return function(t) { displayYear(year(t)); };
-  }
-
-  // Updates the display to show the specified year.
-  function displayYear(year) {
-    dot.data(interpolateData(year), nome).call(position).sort(order);
-    label.text(periodos[Math.round(year)]);
-  }
-
-  // Interpolates the dataset for the given (fractional) year.
-  function interpolateData(year) {
-    return partidos.map(function(d) {
-      return {
-        nome: d.nome,
-        cor: d.cor,
-        tamanho: interpolateValues(d.tamanho, year),
-        x: interpolateValues(d.x, year),
-        y: interpolateValues(d.y, year)
-      };
-    });
-  }
-
-  // Finds (and possibly interpolates) the value for the specified year.
-  function interpolateValues(values, year) {
-    var i = bisect.left(values, year, 0, values.length - 1),
-        a = values[i];
-    if (i > 0) {
-      var b = values[i - 1],
-          t = (year - a[0]) / (b[0] - a[0]);
-      return a[1] * (1 - t) + b[1] * t;
-    }
-    return a[1];
-  }
-}
+  return {
+    initialize:initialize
+  };
+})(jQuery);
