da2c16be86a6811a3098ef40e54a50e4d701af47
Alê Borba <alexandre.borba@imasters.com.br>
2013-04-26 15:36:48 -0300

    Criando requirements.txt
    Colocando as dependencias python em um unico arquivo

--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 #Instalação das dependências no Ubuntu
 
-sudo apt-get install python-numpy python-matplotlib python-pip sqlite3 python-mysqldb
-sudo pip install svgwrite
-wget "http://www.djangoproject.com/download/1.4/tarball/" -O Django-1.4.tar.gz
-tar xzvf Django-1.4.tar.gz
-cd Django-1.4
-sudo python setup.py install 
+sudo apt-get install sqlite3 python-pip
+sudo pip install -r requirements.txt
