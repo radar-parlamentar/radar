87d9e1383c010b8ef7c7215f1aec41ccdcaeabab
David Carlos + Luciano Hideaky <ddavidcarlos1392@gmail.com>
2013-05-17 12:34:11 -0300

    Testes para verificar arquivos criados de exportadores.exportar

--- radar_parlamentar/exportadores/tests.py
+++ radar_parlamentar/exportadores/tests.py
@@ -8,7 +8,7 @@ Replace this with more appropriate tests for your application.
 from __future__ import unicode_literals
 from django.test import TestCase
 from django.core import serializers
-import exportar
+from exportadores import exportar
 import os
 from modelagem import models
 
@@ -16,14 +16,50 @@ from modelagem import models
 MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class exportarTest (TestCase):
+class ExportadoresFileTest (TestCase):
+    
     def test_create_file_partido(self):
+    	exportar.serialize_partido()
+    	filepath = os.path.join(MODULE_DIR, 'dados/partido.xml')
+    	self.assertTrue(os.path.isfile(filepath))
+	
+	def test_create_file_casa_legislativa(self):
+		exportar.serialize_casa_legislativa()
+		filepath = os.path.join(MODULE_DIR, 'dados/casa_legislativa.xml')
+		self.assertTrue(os.path.isfile(filepath))
+    
+    def test_create_file_parlamentar(self):
+    	exportar.serialize_parlamentar()
+    	filepath = os.path.join(MODULE_DIR, 'dados/parlamentar.xml')
+    	self.assertTrue(os.path.isfile(filepath))
+    	
+    def test_create_file_legislatura(self):
+    	exportar.serialize_legislatura()
+    	filepath = os.path.join(MODULE_DIR, 'dados/legislatura.xml')
+    	self.assertTrue(os.path.isfile(filepath))
+    	
+    def test_create_file_proposicao(self):
+    	exportar.serialize_proposicao()
+    	filepath = os.path.join(MODULE_DIR, 'dados/proposicao.xml')
+    	self.assertTrue(os.path.isfile(filepath))
+    	
+    def test_create_file_votacao(self):
+    	exportar.serialize_votacao()
+    	filepath = os.path.join(MODULE_DIR, 'dados/votacao.xml')
+    	self.assertTrue(os.path.isfile(filepath))
+    	
+    def test_create_file_voto(self):
+    	exportar.serialize_voto()
     	filepath = os.path.join(MODULE_DIR, 'dados/voto.xml')
     	self.assertTrue(os.path.isfile(filepath))
-		
 
 
-
+class DadosExportardosTest (TestCase):
+	
+	def test_create_file_partido(self):
+    	exportar.serialize_partido()
+    	filepath = os.path.join(MODULE_DIR, 'dados/partido.xml')
+    	self.assertTrue(os.path.isfile(filepath))
 
         
 
