a139fa186747e77a2e7a4a8a7bcfd0cc5d904d61
Gustavo Correia + Luciano <gus20058@hotmail.com>
2013-05-17 10:04:52 -0300

    Cirado o importador interno, não foi testado ainda

---
+++ radar_parlamentar/importadores/importador_interno.py
@@ -0,0 +1,83 @@
+from django.core import serializers
+from modelagem import models
+import re
+import sys
+import os
+
+MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def deserialize_partido():
+	XMLSerializer = serializers.get_serializer("xml")
+	xml_serializer = XMLSerializer()
+	filepath = os.path.join(MODULE_DIR, 'dados/partido.xml')
+	out = open(filepath, "r")
+	data = xml_serializer.getvalue()
+	
+	for partido in serializers.deserialize("xml", data):
+    		partido.save()	
+
+def deserialize_casa_legislativa():
+	XMLSerializer = serializers.get_serializer("xml")
+	xml_serializer = XMLSerializer()
+	filepath = os.path.join(MODULE_DIR, 'dados/casa_legislativa.xml')
+	out = open(filepath, "r")
+	data = xml_serializer.getvalue()
+	
+	for casa_legislativa in serializers.deserialize("xml", data):
+    		casa_legislativa.save()
+
+def deserialize_parlamentar():
+	XMLSerializer = serializers.get_serializer("xml")
+	xml_serializer = XMLSerializer()
+	filepath = os.path.join(MODULE_DIR, 'dados/parlamentar.xml')
+	out = open(filepath, "r")
+	data = xml_serializer.getvalue()
+	
+	for parlamentar in serializers.deserialize("xml", data):
+    		parlamentar.save()
+
+def deserialize_legislatura():
+	XMLSerializer = serializers.get_serializer("xml")
+	xml_serializer = XMLSerializer()
+	filepath = os.path.join(MODULE_DIR, 'dados/legislatura.xml')
+	out = open(filepath, "r")
+	data = xml_serializer.getvalue()
+	
+	for legislatura in serializers.deserialize("xml", data):
+    		legislatura.save()
+
+def deserialize_proposicao():
+	XMLSerializer = serializers.get_serializer("xml")
+	xml_serializer = XMLSerializer()
+	filepath = os.path.join(MODULE_DIR, 'dados/proposicao.xml')
+	out = open(filepath, "r")
+	data = xml_serializer.getvalue()
+	
+	for proposicao in serializers.deserialize("xml", data):
+    		proposicao.save()
+
+def deserialize_votacao():
+	XMLSerializer = serializers.get_serializer("xml")
+	xml_serializer = XMLSerializer()
+	filepath = os.path.join(MODULE_DIR, 'dados/votacao.xml')
+	out = open(filepath, "r")
+	data = xml_serializer.getvalue()
+	
+	for votacao in serializers.deserialize("xml", data):
+    		votacao.save()
+
+def deserialize_voto():
+	XMLSerializer = serializers.get_serializer("xml")
+	xml_serializer = XMLSerializer()
+	filepath = os.path.join(MODULE_DIR, 'dados/voto.xml')
+	out = open(filepath, "r")
+	data = xml_serializer.getvalue()
+	
+	for voto in serializers.deserialize("xml", data):
+    		voto.save()
+
+
+
+
+
+	
