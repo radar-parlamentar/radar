a03cd33105607488f72c4f214d463ea355193820
David Carlos <ddavidcarlos1392@gmail.com>
2013-05-18 17:28:26 -0300

    testando a exportação de partido.xml

--- radar_parlamentar/exportadores/tests.py
+++ radar_parlamentar/exportadores/tests.py
@@ -16,14 +16,43 @@ from modelagem import models
 MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class ExportadoresFileTest (TestCase):
-    
+
+
+
+class ExportadoresFileTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        partidoTest1 = models.Partido()
+        partidoTest1.nome = 'PMDB'
+        partidoTest1.numero = '15'
+        partidoTest2 = models.Partido()
+        partidoTest2.nome = 'PT'
+        partidoTest2.numero = '13'
+        partidoTest3 = models.Partido()
+        partidoTest3.nome = 'PSDB'
+        partidoTest3.numero = '23'
+        partidoTest1.save()
+        partidoTest2.save()
+        partidoTest3.save()
+
+        
     def test_create_file_partido(self):
-    	exportar.serialize_partido()
+        exportar.serialize_partido()
     	filepath = os.path.join(MODULE_DIR, 'dados/partido.xml')
     	self.assertTrue(os.path.isfile(filepath))
 	
-	def test_create_file_casa_legislativa(self):
+    def test_verify_file_partido(self):
+        partido = models.Partido.objects.get(nome = 'PMDB')
+        filepath = os.path.join(MODULE_DIR, 'dados/partido.xml')
+        file_xml = open(filepath,'r')
+        file_read = file_xml.read()
+        self.assertTrue(file_read.find(partido.nome) > 0)
+        self.assertTrue(file_read.find(str(partido.numero)) > 0)
+
+
+
+
+    def test_create_file_casa_legislativa(self):
 		exportar.serialize_casa_legislativa()
 		filepath = os.path.join(MODULE_DIR, 'dados/casa_legislativa.xml')
 		self.assertTrue(os.path.isfile(filepath))
@@ -54,7 +83,7 @@ class ExportadoresFileTest (TestCase):
     	self.assertTrue(os.path.isfile(filepath))
 
 
-class DadosExportardosTest (TestCase):
+#class DadosExportardosTest (TestCase):
 	
 	
 
