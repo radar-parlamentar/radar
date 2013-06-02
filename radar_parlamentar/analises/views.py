b2c9a6262c99d1f23e8c972c95c73ca921c82ca6
Saulo Trento <saulotrento@gmail.com>
2013-06-01 22:09:55 -0300

    BD sqlite atualizado com as tabelas do novo JSON.
    O BD já contém duas análises: uma da cdep e uma da cmsp, ambas por biênio.
    Para apagar as análises do bd (caso queira refazer), usar o script excluir_analises_do_bd.sh.

--- radar_parlamentar/analises/views.py
+++ radar_parlamentar/analises/views.py
@@ -45,7 +45,7 @@ def json_analise(request,nome_curto_casa_legislativa):
     """Retorna (novo) JSON com dados da análise solicitada."""
 
     casa = get_object_or_404(models.CasaLegislativa,nome_curto=nome_curto_casa_legislativa)
-    at = AnalisadorTemporal(casa,periodicidade=models.ANO,votacoes=[])
+    at = AnalisadorTemporal(casa,periodicidade=models.BIENIO,votacoes=[])
     # O argumento votacoes passado em branco irá utilizar todas as votações.
     # Se for uma lista de votações, serão consideras apenas estas.
     json = at.get_json()
