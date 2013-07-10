6a01fd8180137e2d36fdba93cc0aa0be7f85601d
David Carlos <ddavidcarlos1392@gmail.com>
2013-07-10 18:33:49 -0300

    Salvando no banco uma casaLegislativa a partir do xml de exemplo.

--- radar_parlamentar/importadorInterno/novo_importador.py
+++ radar_parlamentar/importadorInterno/novo_importador.py
@@ -3,28 +3,32 @@ from django.core import serializers
 import xml.etree.ElementTree as etree
 import urllib2
 import os
-
+from modelagem import models
 
 MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 RESOURCES_FOLDER = os.path.join(MODULE_DIR, '../exportadores/dados/EXEMPLO.xml')
 
 
+
 class importador_interno:
+
 
 	@staticmethod
 	def carrega_xml():
-		diretorio = RESOURCES_FOLDER
-		try:	
-			#xml = open(diretorio,'r')
-			tree = etree.parse(diretorio)
-			root = tree.getroot()
-			print root.tag, root.attrib
-			for child in root:
-				print child.tag, child.attrib
-		except IOError as e:
-			print "I/O erro, nao ha nenhum arquivo de Partido para ser importado".format(e.errno, e.strerror)
-			return
-
+		diretorio = RESOURCES_FOLDER	
+		tree = etree.parse(diretorio)
+		root = tree.getroot()
+		for child in root:
+			print root.tag,root.attrib
+			print child.tag	
+		#dicionario = dict(root)
+		casaLegislativa = models.CasaLegislativa()
+		casaLegislativa.nome_curto = root.attrib.get("nome_curto")
+		casaLegislativa.nome = root.attrib.get("nome")
+		casaLegislativa.esfera = root.attrib.get("esfera")
+		casaLegislativa.local = root.attrib.get("local")
+		casaLegislativa.atualizacao = root.attrib.get("atualizacao")
+		casaLegislativa.save()	
 
 def main():
 	importador_interno.carrega_xml()
