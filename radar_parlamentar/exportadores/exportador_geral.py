a7b8caea1625f26cfe320f40d08f45d89d94cb22
Arthur Del Esposte <arthurmde@yahoo.com.br>
2013-07-10 10:10:32 -0300

    Limpeza do código do exportador_geral

--- radar_parlamentar/exportadores/exportador_geral.py
+++ radar_parlamentar/exportadores/exportador_geral.py
@@ -28,15 +28,6 @@ import os
 
 MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-def main():
-	serialize_partido()
-	serialize_casa_legislativa()
-	serialize_parlamentar()
-	serialize_legislatura()
-	serialize_proposicao()
-	serialize_votacao()
-	serialize_voto()
-
 def serialize_casa_legislativa(nome_curto):
 
 	#Identificando Casa
@@ -45,9 +36,7 @@ def serialize_casa_legislativa(nome_curto):
 		raise ValueError('Casa Legislativa não encontrada\n')
 
 	root = Element('CasaLegislativa', nome = casa[0].nome, nome_curto = casa[0].nome_curto, esfera = casa[0].esfera, local = casa[0].local, atualizacao = str(casa[0].atualizacao))
-
-	#casa_xml = '<CasaLegislativa nome="' + casa[0].nome + '" nome_curto="' + casa[0].nome_curto + '" esfera="' + casa[0].esfera + '" local="' + casa[0].local + '" atualizacao="' + str(casa[0].atualizacao) + '">'
-    
+   
 	#Identificando a proposição
 	proposicao = models.Proposicao.objects.filter(casa_legislativa_id__nome_curto = nome_curto)
 
@@ -80,67 +69,3 @@ def serialize_casa_legislativa(nome_curto):
 	out = open(filepath, "w")
 	ElementTree(root).write(out)
 	out.close()
-
-
-def serialize_partido():
-	XMLSerializer = serializers.get_serializer("xml")
-	xml_serializer = XMLSerializer()
-	filepath = os.path.join(MODULE_DIR, 'dados/partido.xml')
-	out = open(filepath, "w")
-	xml_serializer.serialize(models.Partido.objects.all(), stream=out)
-	data = xml_serializer.getvalue()
-'''
-def serialize_casa_legislativa():
-	XMLSerializer = serializers.get_serializer("xml")
-	xml_serializer = XMLSerializer()
-	out = open(os.path.join(MODULE_DIR, 'dados/casa_legislativa.xml'), "w")
-	xml_serializer.serialize(models.CasaLegislativa.objects.all(), stream=out)
-	data = xml_serializer.getvalue()
-'''	
-def serialize_parlamentar():
-	XMLSerializer = serializers.get_serializer("xml")
-	xml_serializer = XMLSerializer()
-	out = open(os.path.join(MODULE_DIR, 'dados/parlamentar.xml'), "w")
-	xml_serializer.serialize(models.Parlamentar.objects.all(), stream=out)
-	data = xml_serializer.getvalue()
-		
-def serialize_legislatura():
-	XMLSerializer = serializers.get_serializer("xml")
-	xml_serializer = XMLSerializer()
-	out = open(os.path.join(MODULE_DIR, 'dados/legislatura.xml'), "w")
-	xml_serializer.serialize(models.Legislatura.objects.all(), stream=out)
-	data = xml_serializer.getvalue()
-	
-def serialize_proposicao():
-	XMLSerializer = serializers.get_serializer("xml")
-	xml_serializer = XMLSerializer()
-	out = open(os.path.join(MODULE_DIR, 'dados/proposicao.xml'), "w")
-	xml_serializer.serialize(models.Proposicao.objects.all(), stream=out)
-	data = xml_serializer.getvalue()
-	
-def serialize_votacao():
-	XMLSerializer = serializers.get_serializer("xml")
-	xml_serializer = XMLSerializer()
-	out = open(os.path.join(MODULE_DIR, 'dados/votacao.xml'), "w")
-	xml_serializer.serialize(models.Votacao.objects.all(), stream=out)
-	data = xml_serializer.getvalue()
-	
-def serialize_voto():
-	XMLSerializer = serializers.get_serializer("xml")
-	xml_serializer = XMLSerializer()
-	out = open(os.path.join(MODULE_DIR, 'dados/voto.xml'), "w")
-	xml_serializer.serialize(models.Voto.objects.all(), stream=out)
-	data = xml_serializer.getvalue()
-	
-
-	
-
-
-
-
-
-	
-
-
-	
-
