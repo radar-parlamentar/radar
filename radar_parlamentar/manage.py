8f75afc6616c6e0c64bd35f6c0b80f8f5f35a43e
Alan Fachini <alfakini@gmail.com>
2013-07-22 19:28:57 -0300

    Evitando erro caso nao passe nenhum argumento para o manage.py.

--- radar_parlamentar/manage.py
+++ radar_parlamentar/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    if sys.argv[1] == 'test':
+    if (len(sys.argv) >= 2) and (sys.argv[1] == 'test'):
         os.environ["DJANGO_SETTINGS_MODULE"] = "settings.test"
     else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
