496728c357bd77af63209bbabdadc261997e5db4
Alan Fachini <alfakini@gmail.com>
2013-07-21 21:49:50 -0300

    Fix manage.py para usar a config settings.test quando rodar o ambiente de teste.

--- radar_parlamentar/manage.py
+++ radar_parlamentar/manage.py
@@ -3,8 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radar_parlamentar.settings.development")
+    if sys.argv[1] == 'test':
+        os.environ["DJANGO_SETTINGS_MODULE"] = "radar_parlamentar.settings.test"
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radar_parlamentar.settings.development")
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
