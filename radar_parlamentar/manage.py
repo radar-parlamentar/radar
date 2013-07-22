c1499cfe346f9245e5b6524c15652e0dcf42800a
Alan Fachini <alfakini@gmail.com>
2013-07-21 21:53:09 -0300

    Move settings para o root do projeto django.

--- radar_parlamentar/manage.py
+++ radar_parlamentar/manage.py
@@ -4,9 +4,9 @@ import sys
 
 if __name__ == "__main__":
     if sys.argv[1] == 'test':
-        os.environ["DJANGO_SETTINGS_MODULE"] = "radar_parlamentar.settings.test"
+        os.environ["DJANGO_SETTINGS_MODULE"] = "settings.test"
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radar_parlamentar.settings.development")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
