371a65d1ba95f4ffeea6cf7290c9d4cc6304be96
leonardofl <leonardofl87@gmail.com>
2013-07-24 16:06:08 -0300

    adequando tests.sh para travis

--- radar_parlamentar/tests.sh
+++ radar_parlamentar/tests.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-python manage.py test modelagem
-python manage.py test analises
-python manage.py test importadores
-python manage.py test exportadores
+python manage.py test modelagem && python manage.py test analises
