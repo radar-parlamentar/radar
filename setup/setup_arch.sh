ca57b5e016b5a702243cb5c92db67d67da407f28
Eduardo Kuroda <eduardohideo@gmail.com>
2013-09-26 02:33:13 -0300

    incluida a lib mock no setup

--- setup/setup_arch.sh
+++ setup/setup_arch.sh
@@ -3,7 +3,7 @@
 
 su
 pacman -S python2 python2-numpy python2-matplotlib python2-pip sqlite3 mysql-python
-pip2 install svgwrite
+pip2 install svgwrite mock
 wget "http://www.djangoproject.com/download/1.4/tarball/" -O Django-1.4.tar.gz
 tar xzvf Django-1.4.tar.gz
 cd Django-1.4
--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Instalação das dependências no Ubuntu
 
-sudo apt-get install sqlite3 python-pip
+sudo apt-get install sqlite3 python-pip python-mock
 sudo pip install -r requirements.txt
 mkdir /tmp/django_cache
