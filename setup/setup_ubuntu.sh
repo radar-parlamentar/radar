e740ce954e7e121e111d5242359591ad9e5c435c
leonardofl <leonardofl87@gmail.com>
2013-10-09 14:36:29 -0300

    updating setup scripts

--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Instalação das dependências no Ubuntu
 
-sudo apt-get install sqlite3 python-pip python-mock
+sudo apt-get install sqlite3 python-pip 
 sudo pip install -r requirements.txt
 mkdir /tmp/django_cache
