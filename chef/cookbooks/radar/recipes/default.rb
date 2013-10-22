e4084c4c217d305796a67a22bca90a4850cdcd26
leonardofl <leonardofl87@gmail.com>
2013-10-21 23:54:39 -0200

    começando a trabalhar na receita chef do radar

---
+++ chef/cookbooks/radar/recipes/default.rb
@@ -0,0 +1,58 @@
+# License: GPL v3
+# Author: Leonardo Leite (2013)
+
+include_recipe "mysql::server"
+include_recipe "database::mysql"
+
+mysql_database 'radarparlamentar' do
+  connection(
+    :host     => 'localhost',
+    :username => 'root',
+    :password => node['mysql']['server_root_password']
+  )
+  action :create
+end
+
+mysql_connection_info = {
+  :host     => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
+
+mysql_database_user 'radarparlamentar' do
+  connection    mysql_connection_info
+  password      node['radar']['database_user_password']
+  database_name 'radarparlamentar'
+  host          '%'
+  privileges    [:select,:update,:insert]
+  action        :grant
+end
+
+#application 'radar_parlamentar' do
+#  path       '$HOME/radar_parlamentar'
+#  owner      'nobody'
+#  group      'nogroup'
+#  repository 'https://github.com/leonardofl/radar_parlamentar.git'
+#  revision   'master'
+#
+#  django do
+#    settings_template 'settings.py.erb'
+#    debug             true
+#    collectstatic     'build_static --noinput'
+#    database do
+#      database 'radarparlamentar'
+#      adapter  'mysql'
+#      username 'radarparlamentar'
+#      password '!!!'
+#    end
+#  end
+#
+#  gunicorn do
+#    only_if { node['roles'].include? 'packaginator_application_server' }
+#    app_module :django
+#    port 8080
+#  end
+#
+#end
+
+
---
+++ chef/solo.rb
@@ -0,0 +1,4 @@
+file_cache_path "/tmp/chef-solo"
+cookbook_path "/tmp/chef-solo/cookbooks"
+json_attribs "/tmp/chef-solo/node.json"
+
