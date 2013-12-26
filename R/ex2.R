e28c25e6bc40e1714598d80a51aebc082fc6ec49
leonardofl <leonardofl87@gmail.com>
2013-12-26 16:15:33 -0200

    lop=-1 e minvotes=0 em ex2.R

--- R/ex2.R
+++ R/ex2.R
@@ -1,7 +1,7 @@
 source("radar.R")
 
 roll_call <- build_rollcall('sen2005-2006.csv', 'Brazilian Senate 2005-2006')
-radar_pca <- radarpca(roll_call, lop = 0, minvotes = 2, scale=FALSE, center=FALSE)
+radar_pca <- radarpca(roll_call, lop = -1, minvotes = 0, scale=FALSE, center=FALSE)
 plot_radar_black_and_white(radar_pca)
 plot_radar(radar_pca)
 
