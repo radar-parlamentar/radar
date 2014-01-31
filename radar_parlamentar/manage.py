7515698ceedf39e3c768626071f6b10d31d49b4d
leonardofl <leonardofl87@gmail.com>
2014-01-31 07:18:05 -0200

    fixing manage.py

--- radar_parlamentar/manage.py
+++ radar_parlamentar/manage.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     if (len(sys.argv) >= 2) and (sys.argv[1] == 'test'):
         os.environ["DJANGO_SETTINGS_MODULE"] = "settings.test"
     else:
-        if not os.environ["DJANGO_SETTINGS_MODULE"]:
+        print "DJANGO_SETTINGS_MODULE" in os.environ
+        if not "DJANGO_SETTINGS_MODULE" in os.environ:
             os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 
     from django.core.management import execute_from_command_line
