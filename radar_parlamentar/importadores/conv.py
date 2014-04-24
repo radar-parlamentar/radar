97fbcc7026b959ca87e96cd3b06f865dae881d63
carol15022 <carol15022@hotmail.com>
2014-04-24 15:44:24 -0300

    Mudança do nome da casa para seu nome curto
    Signed-off-by Carolina Barros <carol15022@hotmail.com>
    Signed-off-by Eduardo Barbosa <eduduarduo@hotmail.com>

---
+++ radar_parlamentar/importadores/conv.py
@@ -0,0 +1,281 @@
+#!/usr/bin/python
+# coding=utf8
+
+# Copyright (C) 2012, Leonardo Leite
+#
+# This file is part of Radar Parlamentar.
+# 
+# Radar Parlamentar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Radar Parlamentar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Radar Parlamentar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""módulo convencao (Convenção Nacional Francesa)
+
+Classes:
+    ImportadorConvencao gera dados para casa legislativa fictícia chamada Convenção Nacional Francesa
+"""
+
+from __future__ import unicode_literals
+from django.utils.dateparse import parse_datetime
+from modelagem import models
+
+ULTIMA_ATUALIZACAO = parse_datetime('2012-06-01 0:0:0')
+
+# Eu queria deixar as datas no século de 1700, mas o datetime só lida com datas a partir de 1900
+INICIO_PERIODO = parse_datetime('1989-01-01 0:0:0')
+FIM_PERIODO = parse_datetime('1989-12-30 0:0:0')
+
+DATA_NO_PRIMEIRO_SEMESTRE = parse_datetime('1989-02-02 0:0:0')
+DATA_NO_SEGUNDO_SEMESTRE = parse_datetime('1989-10-10 0:0:0')
+
+PARLAMENTARES_POR_PARTIDO = 3
+
+GIRONDINOS = 'Girondinos'
+JACOBINOS = 'Jacobinos'
+MONARQUISTAS = 'Monarquistas'
+
+class ImportadorConvencao:
+    
+    def _gera_casa_legislativa(self):
+        conv = models.CasaLegislativa()
+        conv.nome = 'Convenção Nacional Francesa'
+        conv.nome_curto = 'conv'
+        conv.esfera = models.FEDERAL
+        conv.local = 'Paris (FR)'
+        conv.atualizacao = ULTIMA_ATUALIZACAO
+        conv.save()
+        return conv
+    
+    def _gera_partidos(self):
+        girondinos = models.Partido()
+        girondinos.nome = GIRONDINOS
+        girondinos.numero = 27
+        girondinos.cor = '#008000'
+        girondinos.save()
+        jacobinos = models.Partido()
+        jacobinos.nome = JACOBINOS
+        jacobinos.numero = 42
+        jacobinos.cor = '#FF0000'
+        jacobinos.save()
+        monarquistas = models.Partido()
+        monarquistas.nome = MONARQUISTAS
+        monarquistas.numero = 79
+        monarquistas.cor = '#800080'
+        monarquistas.save()
+        self.partidos = {girondinos, jacobinos, monarquistas}
+
+    
+    def _gera_legislaturas(self):
+        self.legs = {} # nome partido => lista de legislaturas do partido
+        for p in self.partidos:
+            self.legs[p.nome] = []
+            for i in range(0, PARLAMENTARES_POR_PARTIDO):
+                
+                parlamentar = models.Parlamentar()
+                parlamentar.id_parlamentar = '%s%s' % (p.nome[0], str(i))
+                parlamentar.nome = 'Pierre'
+                parlamentar.save()
+                
+                leg = models.Legislatura()
+                leg.casa_legislativa = self.casa
+                leg.inicio = INICIO_PERIODO
+                leg.fim = FIM_PERIODO
+                leg.partido = p
+                leg.parlamentar = parlamentar
+                leg.save()
+                self.legs[p.nome].append(leg)
+
+    def _gera_proposicao(self, num, descricao):  
+        prop = models.Proposicao()    
+        prop.id_prop = num
+        prop.sigla = 'PL'
+        prop.numero = num
+        prop.ementa = descricao
+        prop.descricao = descricao
+        prop.casa_legislativa = self.casa
+        prop.save()
+        return prop
+
+    def _gera_votacao(self, num, descricao, data, prop):
+        votacao = models.Votacao()
+        votacao.id_vot = num
+        votacao.descricao = descricao
+        votacao.data = data
+        votacao.proposicao = prop
+        votacao.save()
+        return votacao
+            
+    def _gera_votos(self, votacao, nome_partido, opcoes):
+        # opcoes é uma lista de opções (SIM, NÃO ...)
+        for i in range(0, PARLAMENTARES_POR_PARTIDO):
+            voto = models.Voto()
+            voto.legislatura = self.legs[nome_partido][i]
+            voto.opcao = opcoes[i]
+            voto.votacao = votacao
+            voto.save() 
+            
+    def _gera_votacao1(self):
+        
+        NUM = '1'
+        DESCRICAO = 'Reforma agrária'
+        prop = self._gera_proposicao(NUM, DESCRICAO)
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_PRIMEIRO_SEMESTRE, prop)
+
+        votos_girondinos = [models.SIM, models.ABSTENCAO, models.NAO]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.NAO, models.NAO, models.NAO]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas)
+        
+    def _gera_votacao2(self):
+        
+        NUM = '2'
+        DESCRICAO = 'Aumento da pensão dos nobres'
+        prop = self._gera_proposicao(NUM, DESCRICAO)
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_PRIMEIRO_SEMESTRE, prop)
+
+        votos_girondinos = [models.NAO, models.NAO, models.NAO]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.NAO, models.NAO, models.NAO]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas)       
+        
+    def _gera_votacao3(self):
+        
+        NUM = '3'
+        DESCRICAO = 'Institui o Dia de Carlos Magno'
+        prop = self._gera_proposicao(NUM, DESCRICAO)
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_PRIMEIRO_SEMESTRE, prop)
+
+        votos_girondinos = [models.NAO, models.NAO, models.SIM]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.NAO, models.NAO, models.NAO]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas) 
+        
+    def _gera_votacao4(self):
+        
+        NUM = '4'
+        DESCRICAO = 'Diminuição de impostos sobre a indústria'
+        prop = self._gera_proposicao(NUM, DESCRICAO)
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_PRIMEIRO_SEMESTRE, prop)
+
+        votos_girondinos = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.SIM, models.ABSTENCAO, models.NAO]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.SIM, models.NAO, models.AUSENTE]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas) 
+        
+    def _gera_votacao5(self):
+        
+        NUM = '5'
+        DESCRICAO = 'Guilhotinar o Conde Pierre'
+        prop = self._gera_proposicao(NUM, DESCRICAO)
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_SEGUNDO_SEMESTRE, prop)
+
+        votos_girondinos = [models.SIM, models.SIM, models.ABSTENCAO]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.NAO, models.NAO, models.NAO]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas) 
+        
+    def _gera_votacao6(self):
+        
+        NUM = '6'
+        DESCRICAO = 'Criação de novas escolas'
+        prop = self._gera_proposicao(NUM, DESCRICAO)
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_SEGUNDO_SEMESTRE, prop)
+
+        votos_girondinos = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.AUSENTE, models.SIM, models.SIM]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas) 
+        
+    def _gera_votacao7(self):
+        
+        NUM = '7'
+        DESCRICAO = 'Aumento do efetivo militar'
+        prop = self._gera_proposicao(NUM, DESCRICAO)
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_SEGUNDO_SEMESTRE, prop)
+
+        votos_girondinos = [models.SIM, models.SIM, models.ABSTENCAO]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.SIM, models.SIM, models.SIM]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.SIM, models.AUSENTE, models.SIM]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas) 
+    
+    # votação com atributos diferentes para teste
+    def _gera_votacao8(self):
+        
+        NUM = '8'
+        DESCRICAO = 'Guerra contra a Inglaterra'
+        prop = models.Proposicao()    
+        prop.id_prop = NUM
+        prop.sigla = 'PL'
+        prop.numero = NUM
+        prop.ementa = 'o uso proibido de armas químicas'
+        prop.descricao = 'descricao da guerra'
+        prop.casa_legislativa = self.casa
+        prop.indexacao = 'bombas, efeitos, destruições'
+        prop.save()
+        votacao = self._gera_votacao(NUM, DESCRICAO, DATA_NO_SEGUNDO_SEMESTRE, prop)
+
+        votos_girondinos = [models.NAO, models.NAO, models.NAO]
+        self._gera_votos(votacao, GIRONDINOS, votos_girondinos)
+
+        votos_jacobinos = [models.ABSTENCAO, models.NAO, models.NAO]
+        self._gera_votos(votacao, JACOBINOS, votos_jacobinos)
+
+        votos_monarquistas = [models.SIM, models.AUSENTE, models.SIM]
+        self._gera_votos(votacao, MONARQUISTAS, votos_monarquistas) 
+
+    def importar(self):
+        self.casa = self._gera_casa_legislativa()
+        self._gera_partidos()
+        self._gera_legislaturas()
+        self._gera_votacao1()
+        self._gera_votacao2()
+        self._gera_votacao3()
+        self._gera_votacao4()
+        self._gera_votacao5()
+        self._gera_votacao6()
+        self._gera_votacao7()
+        self._gera_votacao8()
+        self._gera_proposicao('9', 'Legalizacao da maconha')
+    
+def main():
+
+    print 'IMPORTANDO DADOS DA CONVENÇÃO NACIONAL FRANCESA'
+    importer = ImportadorConvencao()
+    importer.importar()
---
+++ radar_parlamentar/importadores/sen.py
@@ -0,0 +1,394 @@
+#!/usr/bin/python
+# coding=utf8
+
+# Copyright (C) 2012, Leonardo Leite
+#
+# This file is part of Radar Parlamentar.
+# 
+# Radar Parlamentar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Radar Parlamentar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Radar Parlamentar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""módulo senado
+
+Classes:
+    CasaLegislativaGerador
+    ImportadorVotacoesSenado
+    ImportadorSenadores
+"""
+
+from __future__ import unicode_literals
+from django.utils.dateparse import parse_datetime
+from datetime import datetime, timedelta, date
+from modelagem import models
+import urllib2
+import re
+import os
+import xml.etree.ElementTree as etree
+import logging
+
+# data em que os arquivos XMLs foram atualizados
+ULTIMA_ATUALIZACAO = parse_datetime('2013-02-14 0:0:0')
+
+MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# pasta com os arquivos com os dados fornecidos pelo senado
+DATA_FOLDER = os.path.join(MODULE_DIR, 'dados/senado')
+VOTACOES_FOLDER = os.path.join(DATA_FOLDER, 'votacoes')
+
+NOME_CURTO = 'sen'
+
+logger = logging.getLogger("radar")
+
+class SenadoWS:
+    """Acesso aos web services do senado"""
+    
+    URL_LEGISLATURA = 'http://legis.senado.gov.br/dadosabertos/senador/lista/legislatura/%s'
+    
+    def obter_senadores_from_legislatura(self, id_leg):
+        """Obtém senadores de uma legislatura
+
+        Argumentos:
+        id_leg
+
+        Retorna:
+        Um objeto ElementTree correspondente ao XML retornado pelo web service
+        Exemplo: http://legis.senado.gov.br/dadosabertos/senador/lista/legislatura/52
+
+        Exceções:
+            ValueError -- quando legislatura não existe
+        """
+        url = SenadoWS.URL_LEGISLATURA % id_leg
+        try:
+            request = urllib2.Request(url)
+            xml = urllib2.urlopen(request).read()
+        except urllib2.URLError:
+            raise ValueError('Legislatura %s não encontrada' % id_leg)
+
+        try:
+            tree = etree.fromstring(xml)
+        except etree.ParseError:
+            raise ValueError('Legislatura %s não encontrada' % id_leg)
+
+        return tree
+
+
+class CasaLegislativaGerador:
+    
+    def gera_senado(self):
+        """Gera objeto do tipo CasaLegislativa representando o Senado"""
+
+        if not models.CasaLegislativa.objects.filter(nome_curto=NOME_CURTO):
+            sen = models.CasaLegislativa()
+            sen.nome = 'Senado'
+            sen.nome_curto = NOME_CURTO
+            sen.esfera = models.FEDERAL
+            sen.atualizacao = ULTIMA_ATUALIZACAO
+            sen.save()
+        else:
+            sen = models.CasaLegislativa.objects.get(nome_curto=NOME_CURTO)
+        return sen
+
+class ImportadorVotacoesSenado:
+    """Salva os dados dos arquivos XML do senado no banco de dados"""
+
+    def __init__(self):
+        self.senado = models.CasaLegislativa.objects.get(nome_curto=NOME_CURTO)
+        self.proposicoes = {} # chave é o nome da proposição (sigla num/ano), valor é objeto Proposicao
+
+    def _converte_data(self, data_str):
+        """Converte string "aaaa-mm-dd para objeto datetime; retona None se data_str é inválido"""
+        DATA_REGEX = '(\d{4})-(\d{2})-(\d{2})'
+        res = re.match(DATA_REGEX, data_str)
+        if res:
+            return date(int(res.group(1)), int(res.group(2)), int(res.group(3)))
+        else:
+            raise ValueError
+        
+    def _voto_senado_to_model(self, voto):
+        """Interpreta voto como tá no XML e responde em adequação a modelagem em models.py"""
+        
+        DESCULPAS = ['MIS', 'MERC', 'P-NRV', 'REP', 'AP', 'LA', 'LAP', 'LC', 'LG', 'LS', 'NA']
+        
+        if voto == 'Não':          # XML não tá em UTF-8, acho q vai dar probema nessas comparações!
+            return models.NAO
+        elif voto == 'Sim':
+            return models.SIM
+        elif voto == 'NCom':
+            return models.AUSENTE
+        elif voto in DESCULPAS:
+            return models.AUSENTE
+        elif voto == 'Abstenção':
+            return models.ABSTENCAO
+        elif voto == 'P-OD': # obstrução
+            return models.ABSTENCAO
+        else:
+            return models.ABSTENCAO
+
+    def _find_partido(self, nome_partido):
+        
+        nome_partido = nome_partido.strip()
+        partido = models.Partido.from_nome(nome_partido)
+        if partido == None:
+            logger.warn('Não achou o partido %s' % nome_partido)
+            partido = models.Partido.get_sem_partido()
+        return partido
+
+    def _cria_legislatura(self, voto_parlamentar_tree, votacao):
+        
+        nome = voto_parlamentar_tree.find('NomeParlamentar').text
+        codigo = voto_parlamentar_tree.find('CodigoParlamentar').text
+        sexo = voto_parlamentar_tree.find('SexoParlamentar').text
+        
+        if models.Parlamentar.objects.filter(nome=nome, id_parlamentar=codigo).exists():
+            senador = models.Parlamentar.objects.get(nome=nome, id_parlamentar=codigo)
+        else:
+            senador = models.Parlamentar()
+            senador.id_parlamentar = codigo
+            senador.nome = nome
+            senador.genero = sexo
+            senador.save()
+
+        leg = models.Legislatura()
+        leg.parlamentar = senador
+        leg.casa_legislativa = self.senado
+        ini, fim = self._periodo_arbitrario(votacao.data)
+        leg.inicio = ini
+        leg.fim = fim
+        sigla_partido = voto_parlamentar_tree.find('SiglaPartido').text
+        leg.partido = self._find_partido(sigla_partido)
+        leg.localidade = voto_parlamentar_tree.find('SiglaUF').text
+        leg.save()
+        return leg
+        
+    def _votos_from_tree(self, votos_tree, votacao):
+        """Faz o parse dos votos, salva no BD e devolve lista de votos
+           Retorna lista dos votos salvos
+        """
+        votos = []
+        for voto_parlamentar_tree in votos_tree:
+            nome_senador = voto_parlamentar_tree.find('NomeParlamentar').text
+            try:
+                legislatura = models.Legislatura.find(votacao.data, nome_senador)
+            except ValueError:
+                logger.warn('Não encontramos legislatura do senador %s' % nome_senador)
+                logger.info('Criando legislatura para o senador %s' % nome_senador)
+                legislatura = self._cria_legislatura(voto_parlamentar_tree, votacao)
+            voto = models.Voto()
+            voto.legislatura = legislatura
+            voto.votacao = votacao
+            voto.opcao = self._voto_senado_to_model(voto_parlamentar_tree.find('Voto').text)
+            voto.save()
+            votos.append(voto)
+        return votos
+
+    def _periodo_arbitrario(self, dt):
+        """Retorna datas de início e fim de uma legislatura arbitrária que contenha a data informada"""
+        
+        periodo1 = (date(2011, 02, 01), date(2019, 01, 31))
+        periodo2 = (date(2003, 02, 01), date(2011, 01, 31))
+        if dt >= periodo1[0] and dt <= periodo1[1]:
+            return periodo1[0], periodo1[1]
+        elif dt >= periodo2[0] and dt <= periodo2[1]:
+            return periodo2[0], periodo2[1]
+        else:
+            raise ValueError('Data %s inválida' % dt)
+
+    def _nome_prop_from_tree(self, votacao_tree):
+        
+        sigla = votacao_tree.find('SiglaMateria').text
+        numero = votacao_tree.find('NumeroMateria').text
+        ano = votacao_tree.find('AnoMateria').text
+        return '%s %s/%s' % (sigla, numero, ano)
+    
+    def _proposicao_from_tree(self, votacao_tree):
+        
+        prop_nome = self._nome_prop_from_tree(votacao_tree)
+        if self.proposicoes.has_key(prop_nome):
+            prop = self.proposicoes[prop_nome]
+        else:
+            prop = models.Proposicao()
+            prop.sigla = votacao_tree.find('SiglaMateria').text
+            prop.numero = votacao_tree.find('NumeroMateria').text
+            prop.ano = votacao_tree.find('AnoMateria').text
+            prop.casa_legislativa = self.senado
+            prop.save()
+            self.proposicoes[prop_nome] = prop
+        return prop
+        
+
+    def _from_xml_to_bd(self, xml_file):
+        """Salva no banco de dados do Django e retorna lista das votações"""
+
+        f = open(xml_file, 'r')
+        xml = f.read()
+        f.close()
+        tree = etree.fromstring(xml)
+
+        votacoes = []        
+        # Pelo q vimos, nesses XMLs não há votações 'inúteis' (homenagens etc) como na cmsp (exceto as secretas)
+        votacoes_tree = tree.find('Votacoes')
+        if votacoes_tree != None:
+            for votacao_tree in votacoes_tree:
+                if votacao_tree.tag == 'Votacao' and votacao_tree.find('Secreta').text == 'N': # se votação não é secreta
+
+                    codigo = votacao_tree.find('CodigoSessaoVotacao').text
+                    votacoes_query = models.Votacao.objects.filter(id_vot=codigo)
+                    
+                    if votacoes_query:
+                        votacao = votacoes_query[0]
+                        votacoes.append(votacao)
+                    else:
+                        proposicao = self._proposicao_from_tree(votacao_tree)
+                        nome = '%s %s/%s' % (proposicao.sigla, proposicao.numero, proposicao.ano)
+                        logger.debug('Importando %s' % nome)
+                        votacao = models.Votacao()
+                        votacao.id_vot = codigo
+                        votacao.save() # só pra criar a chave primária e poder atribuir o votos
+                        votacao.descricao = votacao_tree.find('DescricaoVotacao').text
+                        votacao.data = self._converte_data(votacao_tree.find('DataSessao').text)
+                        if votacao_tree.find('Resultado') != None:
+                            votacao.resultado = votacao_tree.find('Resultado').text
+                        votacao.proposicao = proposicao
+                        votos_tree = votacao_tree.find('Votos')
+                        if votos_tree != None:
+                            votos = self._votos_from_tree(votos_tree, votacao)
+                            if not votos:
+                                logger.warn('Votação desconsiderada (sem votos)')
+                                votacao.delete()
+                            else:
+                                votacao.save()
+                                votacoes.append(votacao)
+                        else:
+                            logger.warn('Votação desconsiderada (votos_tree nulo)')
+                            votacao.delete()
+        return votacoes
+    
+    def progresso(self):
+        """Indica progresso na tela"""
+        print('.'),
+        
+    def _xml_file_names(self):
+        """Retorna uma lista com os caminhos dos arquivos XMLs contidos na pasta VOTACOES_FOLDER"""
+        files = os.listdir(VOTACOES_FOLDER)
+        xmls = filter(lambda name: name.endswith('.xml'), files)
+        xmls = map(lambda name: os.path.join(VOTACOES_FOLDER, name), xmls)
+        return xmls
+
+    def importar_votacoes(self):
+        #for xml_file in ['importadores/dados/senado/ListaVotacoes2011.xml']: # facilita debug 
+        for xml_file in self._xml_file_names():
+            logger.info('Importando %s' % xml_file)
+            self._from_xml_to_bd(xml_file)
+
+
+
+class ImportadorSenadores:
+    
+    LEGISLATURAS = [52, 53, 54, 55] # essa lista precisa ser atualizado de anos em anos
+                                # 52 é a legislatura mínima porque só temos votacões desde 2005 
+    
+    def __init__(self):
+        self.senado = models.CasaLegislativa.objects.get(nome_curto=NOME_CURTO)
+    
+    def _converte_data2(self, data_str):
+        """Converte string "dd/mm/aaaa para objeto datetime; retona None se data_str é inválido"""
+        DATA_REGEX = '(\d\d?)/(\d\d?)/(\d{4})'
+        res = re.match(DATA_REGEX, data_str)
+        if res:
+            new_str = '%s-%s-%s 0:0:0' % (res.group(3), res.group(2), res.group(1))
+            return parse_datetime(new_str)
+        else:
+            raise ValueError
+
+    def _find_partido(self, nome_partido):
+        if nome_partido != None:
+            nome_partido = nome_partido.strip()
+        partido = models.Partido.from_nome(nome_partido)
+        if partido == None:
+            logger.warn('Não achou o partido %s' % nome_partido)
+            partido = models.Partido.get_sem_partido()
+        return partido
+    
+    def _find_nome_partido(self, partidos_tree):
+        """Por hora retorna o último partido da lista"""
+        # TODO em alguns casos um senador aparece com vários partidos durante a legislatura; oq fazer?
+        for partido_tree in partidos_tree:
+            last_partido_tree = partido_tree
+        return last_partido_tree.find('SiglaPartido').text
+    
+    def processa_legislatura(self, leg_tree):
+        
+        parlamentares_tree = leg_tree.find('Parlamentar').find('Parlamentares')
+        for parlamentar_tree in parlamentares_tree:
+            codigo = parlamentar_tree.find('CodigoParlamentar').text
+            nome = parlamentar_tree.find('NomeParlamentar').text
+            uf = parlamentar_tree.find('SiglaUF').text
+            partidos_tree = parlamentar_tree.find('Partidos')
+            if partidos_tree != None:
+                nome_partido = self._find_nome_partido(partidos_tree)
+            else:
+                logger.warn('Senador %s não possui lista de partidos!' % nome)
+                nome_partido = None
+            ano_inicio = parlamentar_tree.find('AnoInicio').text
+            ano_fim = parlamentar_tree.find('AnoFim').text
+            
+            if nome_partido == 'PC DO B':
+                nome_partido = 'PCdoB'
+            inicio_legislatura = self._converte_data2('01/01/%s' % ano_inicio)
+            fim_legislatura = self._converte_data2('31/12/%s' % ano_fim)
+            partido = self._find_partido(nome_partido)
+            
+            if not models.Legislatura.objects.filter(inicio=inicio_legislatura, fim=fim_legislatura, parlamentar__nome=nome, partido__nome=nome_partido).exists():
+                logger.info('Importando senador %s (%s-%s)' % (nome, nome_partido, uf)) 
+                
+                if models.Parlamentar.objects.filter(nome=nome, id_parlamentar=codigo).exists():
+                    senador = models.Parlamentar.objects.get(nome=nome, id_parlamentar=codigo)
+                else:
+                    senador = models.Parlamentar()
+                    senador.id_parlamentar = codigo
+                    senador.nome = nome
+                    senador.save()
+                
+                leg = models.Legislatura()
+                leg.parlamentar = senador
+                leg.casa_legislativa = self.senado
+                leg.inicio = inicio_legislatura
+                leg.fim = fim_legislatura
+                leg.partido = partido
+                leg.localidade = uf
+                leg.save()
+                
+    
+    def importar_senadores(self):
+        """Cria parlamentares e legislaturas no banco de dados"""
+        
+        senws = SenadoWS()
+        for id_leg in ImportadorSenadores.LEGISLATURAS:
+            logger.info("Importando senadores da legislatura %s" % id_leg)
+            leg_tree = senws.obter_senadores_from_legislatura(id_leg)
+            self.processa_legislatura(leg_tree)
+
+
+def main():
+
+    logger.info('IMPORTANDO DADOS DO SENADO')
+    geradorCasaLeg = CasaLegislativaGerador()
+    geradorCasaLeg.gera_senado()
+    logger.info('IMPORTANDO SENADORES')
+    importer = ImportadorSenadores()
+    importer.importar_senadores()
+    logger.info('IMPORTANDO VOTAÇÕES DO SENADO')
+    importer = ImportadorVotacoesSenado()
+    importer.importar_votacoes()
+
+
