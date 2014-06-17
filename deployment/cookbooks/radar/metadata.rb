c1639986b50119a89d65e66efa8f2a29172cd310
leonardofl <leonardofl87@gmail.com>
2014-06-17 20:40:23 -0300

    Usando Vagrant pra criar uma VM com MariaDB

--- deployment/cookbooks/radar/metadata.rb
+++ deployment/cookbooks/radar/metadata.rb
@@ -5,6 +5,6 @@ description      "Installs Radar Parlamentar"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "mysql"
+depends "mariadb"
 depends "database"
 
--- deployment/cookbooks/radar/recipes/default.rb
+++ deployment/cookbooks/radar/recipes/default.rb
@@ -1,31 +1,31 @@
 # License: GPL v3
 # Author: Leonardo Leite (2013, 2014)
 
-include_recipe "mysql::server" # TODO change to MariaDB
-include_recipe "database::mysql"
+#include_recipe "mariadb::ruby" 
+#include_recipe "database::mysql"
 
-mysql_database 'radar' do
-  connection(
-    :host     => 'localhost',
-    :username => 'root',
-    :password => node['mysql']['server_root_password']
-  )
-  action :create
-end
+#mysql_database 'radar' do
+#  connection(
+#    :host     => 'localhost',
+#    :username => 'root',
+#    :password => node['mariadb']['server_root_password']
+#  )
+#  action :create
+#end
 
-mysql_connection_info = {
-  :host     => 'localhost',
-  :username => 'root',
-  :password => node['mysql']['server_root_password']
-}
+#mysql_connection_info = {
+#  :host     => 'localhost',
+#  :username => 'root',
+#  :password => node['mariadb']['server_root_password']
+#}
 
-mysql_database_user 'radar' do
-  connection    mysql_connection_info
-  password      node['radar']['database_user_password']
-  database_name 'radar'
-  host          '%'
-  privileges    [:select,:update,:insert]
-  action        :grant
-end
+#mysql_database_user 'radar' do
+#  connection    mysql_connection_info
+#  password      node['radar']['database_user_password']
+#  database_name 'radar'
+#  host          '%'
+#  privileges    [:select,:update,:insert]
+#  action        :grant
+#end
 
 
