4617a981d5284aa8a88c5d5f16dc3b60cba51204
leonardofl <leonardofl87@gmail.com>
2014-06-17 21:17:59 -0300

    criando o banco do radar com o Vagrant

--- deployment/cookbooks/radar/metadata.rb
+++ deployment/cookbooks/radar/metadata.rb
@@ -5,6 +5,5 @@ description      "Installs Radar Parlamentar"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "mariadb"
 depends "database"
 
--- deployment/cookbooks/radar/recipes/default.rb
+++ deployment/cookbooks/radar/recipes/default.rb
@@ -1,31 +1,30 @@
 # License: GPL v3
 # Author: Leonardo Leite (2013, 2014)
 
-#include_recipe "mariadb::ruby" 
-#include_recipe "database::mysql"
+include_recipe "database::mysql"
 
-#mysql_database 'radar' do
-#  connection(
-#    :host     => 'localhost',
-#    :username => 'root',
-#    :password => node['mariadb']['server_root_password']
-#  )
-#  action :create
-#end
+mysql_database 'radar' do
+  connection(
+    :host     => 'localhost',
+    :username => 'root',
+    :password => node['mysql']['server_root_password']
+  )
+  action :create
+end
 
-#mysql_connection_info = {
-#  :host     => 'localhost',
-#  :username => 'root',
-#  :password => node['mariadb']['server_root_password']
-#}
+mysql_connection_info = {
+  :host     => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
 
-#mysql_database_user 'radar' do
-#  connection    mysql_connection_info
-#  password      node['radar']['database_user_password']
-#  database_name 'radar'
-#  host          '%'
-#  privileges    [:select,:update,:insert]
-#  action        :grant
-#end
+mysql_database_user 'radar' do
+  connection    mysql_connection_info
+  password      node['radar']['database_user_password']
+  database_name 'radar'
+  host          '%'
+  privileges    [:select,:update,:insert]
+  action        :grant
+end
 
 
