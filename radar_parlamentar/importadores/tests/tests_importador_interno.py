dbc09c1c220eefdd3a1613e5eb1698bddf126834
carol15022 <carol15022@hotmail.com>
2014-06-30 01:39:34 -0300

    Padronização tests_importador_interno.py

--- radar_parlamentar/importadores/tests/tests_importador_interno.py
+++ radar_parlamentar/importadores/tests/tests_importador_interno.py
@@ -7,72 +7,79 @@ Replace this with more appropriate tests for your application.
 
 from __future__ import unicode_literals
 from django.test import TestCase
-from django.core import serializers
 from importadores import importador_interno
-from exportadores import exportador
-import os
 from modelagem import models
+
 
 class ImportadorInternoTest(TestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		
-		#Criando dados fictícios no mock 
-		partidoTest1 = models.Partido(nome = 'PMDB',numero = '40')
-		partidoTest2 = models.Partido(nome = 'PT',numero = '13')
-		partidoTest3 = models.Partido(nome = 'PSDB',numero = '23')
-		partidoTest1.save()
-		partidoTest2.save()
-		partidoTest3.save()
-		
-		parlamentarTest1 = models.Parlamentar(id_parlamentar = '',nome = 'Ivandro Cunha Lima',genero = '')
-		parlamentarTest2 = models.Parlamentar(id_parlamentar = '',nome = 'Fernando Ferro',genero = '')
-		parlamentarTest3 = models.Parlamentar(id_parlamentar = '',nome = 'Humberto Costa',genero = '')
-		
-		parlamentarTest1.save()
-		parlamentarTest2.save()
-		parlamentarTest3.save()
+    @classmethod
+    def setUpClass(cls):
 
+        # Criando dados fictícios no mock
+        partidoTest1 = models.Partido(nome='PMDB', numero='40')
+        partidoTest2 = models.Partido(nome='PT', numero='13')
+        partidoTest3 = models.Partido(nome='PSDB', numero='23')
+        partidoTest1.save()
+        partidoTest2.save()
+        partidoTest3.save()
 
-		casa_legislativaTest1 = models.CasaLegislativa(nome = 'Camara dos Deputados',nome_curto = 'cdep',esfera = 'FEDERAL',
-		        local = '', atualizacao = '2012-06-01')
+        parlamentarTest1 = models.Parlamentar(
+            id_parlamentar='', nome='Ivandro Cunha Lima', genero='')
+        parlamentarTest2 = models.Parlamentar(
+            id_parlamentar='', nome='Fernando Ferro', genero='')
+        parlamentarTest3 = models.Parlamentar(
+            id_parlamentar='', nome='Humberto Costa', genero='')
 
-		casa_legislativaTest2 = models.CasaLegislativa(nome = 'Camara Municipal de Sao Paulo',nome_curto = 'cmsp',
-		    esfera = 'MUNICIPAL' ,local = 'Sao Paulo - SP', atualizacao = '2012-12-31')
+        parlamentarTest1.save()
+        parlamentarTest2.save()
+        parlamentarTest3.save()
 
-		casa_legislativaTest1.save()
-		casa_legislativaTest2.save()
+        casa_legislativaTest1 = models.CasaLegislativa(
+            nome='Camara dos Deputados', nome_curto='cdep', esfera='FEDERAL',
+            local='', atualizacao='2012-06-01')
 
-		legislaturaTest1 = models.Legislatura(parlamentar=parlamentarTest1,casa_legislativa = casa_legislativaTest1, inicio='2004-01-01', 		 			fim='2012-07-01',partido=partidoTest1, localidade='PB')
-		legislaturaTest1.save()
+        casa_legislativaTest2 = models.CasaLegislativa(
+            nome='Camara Municipal de Sao Paulo', nome_curto='cmsp',
+            esfera='MUNICIPAL', local='Sao Paulo - SP',
+            atualizacao='2012-12-31')
 
-		#Exportando dados do mock para os xml
-		exportar.serialize_partido()
-		exportar.serialize_parlamentar()
-		exportar.serialize_casa_legislativa()
-		exportar.serialize_legislatura()
+        casa_legislativaTest1.save()
+        casa_legislativaTest2.save()
 
-		#Deletando os registros do mock
-		partidoTest1.delete()
-		partidoTest2.delete()
-		partidoTest3.delete()
+        legislaturaTest1 = models.Legislatura(
+            parlamentar=parlamentarTest1,
+            casa_legislativa=casa_legislativaTest1,
+            inicio='2004-01-01', fim='2012-07-01',
+            partido=partidoTest1, localidade='PB')
+        legislaturaTest1.save()
 
-		parlamentarTest1.delete()
-		parlamentarTest2.delete()
-		parlamentarTest3.delete()
+        # Exportando dados do mock para os xml
+        # exportar.serialize_partido()
+        # exportar.serialize_parlamentar()
+        # exportar.serialize_casa_legislativa()
+        # exportar.serialize_legislatura()
 
-		casa_legislativaTest1.delete()
-		casa_legislativaTest2.delete()
+        # Deletando os registros do mock
+        partidoTest1.delete()
+        partidoTest2.delete()
+        partidoTest3.delete()
 
-		legislaturaTest1.delete()
+        parlamentarTest1.delete()
+        parlamentarTest2.delete()
+        parlamentarTest3.delete()
 
-	def test_importar_partido(self):
-		importador_interno.deserialize_partido()
-		PMDB = Partido.objects.filter(nome = 'PMDB')
-		assertEquals(PMDB.numero,40)
+        casa_legislativaTest1.delete()
+        casa_legislativaTest2.delete()
 
-        @classmethod
-        def tearDownClass(cls):
-                from util_test import flush_db
-                flush_db(cls)
+        legislaturaTest1.delete()
+
+    def test_importar_partido(self):
+        importador_interno.deserialize_partido()
+        PMDB = Partido.objects.filter(nome='PMDB')
+        self.assertEquals(PMDB.numero, 40)
+
+    @classmethod
+    def tearDownClass(cls):
+        from util_test import flush_db
+        flush_db(cls)
