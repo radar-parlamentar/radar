646e0b78d49d08f2b17fc982e0943f19504a2694
carol15022 <carol15022@hotmail.com>
2014-06-30 14:33:31 -0300

    Padronização middleware.py

--- radar_parlamentar/radar_parlamentar/middleware.py
+++ radar_parlamentar/radar_parlamentar/middleware.py
@@ -1,21 +1,23 @@
 class ConsoleExceptionMiddleware:
+
     """Custom middleware to print stack traces on Django console"""
+
     def process_exception(self, request, exception):
         import traceback
         import sys
         exc_info = sys.exc_info()
-        print "######################## Exception #############################"
-        print '\n'.join(traceback.format_exception(*(exc_info or sys.exc_info())))
-        print "################################################################"
-        #print repr(request)
-        #print "################################################################"
-
+        exception = traceback.format_exception(*(exc_info or sys.exc_info()))
+        print "######################## Exception ###########################"
+        print '\n'.join(exception)
+        print "##############################################################"
 
 from django.middleware.cache import UpdateCacheMiddleware
 import re
 
+
 class SmartUpdateCacheMiddleware(UpdateCacheMiddleware):
     STRIP_RE = re.compile(r'\b(_[^=]+=.+?(?:; |$))')
+
     def process_request(self, request):
         cookie = self.STRIP_RE.sub('', request.META.get('HTTP_COOKIE', ''))
         request.META['HTTP_COOKIE'] = cookie
