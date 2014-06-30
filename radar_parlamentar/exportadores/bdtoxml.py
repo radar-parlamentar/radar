040eb651c0b2183117ddf4363f3341e75f02cdbb
carol15022 <carol15022@hotmail.com>
2014-06-30 01:14:55 -0300

    Padronização bdtoxml.py

--- radar_parlamentar/exportadores/bdtoxml.py
+++ radar_parlamentar/exportadores/bdtoxml.py
@@ -1,46 +1,50 @@
-#!/usr/bin/python
+# !/usr/bin/python
 # coding=utf8
 
 # setup the environment
-import os, sys
+import os
+import sys
 sys.path.append(os.pardir)
 os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
+
 class XMLWriter:
+
     """Helper class to write out an xml file"""
+
     def __init__(self, pretty=True):
         """Set pretty to True if you want an indented XML file"""
         self.output = ""
         self.stack = []
         self.pretty = pretty
-        
+
     def open(self, tag):
         """Add an open tag"""
         self.stack.append(tag)
         if self.pretty:
-            self.output += "  "*(len(self.stack) - 1);
+            self.output += "  " * (len(self.stack) - 1)
         self.output += "<" + tag + ">"
         if self.pretty:
             self.output += "\n"
-        
+
     def close(self):
         """Close the innermost tag"""
         if self.pretty:
-            self.output += "\n" + "  "*(len(self.stack) - 1);
+            self.output += "\n" + "  " * (len(self.stack) - 1)
         tag = self.stack.pop()
         self.output += "</" + tag + ">"
         if self.pretty:
             self.output += "\n"
-        
+
     def closeAll(self):
         """Close all open tags"""
         while len(self.stack) > 0:
             self.close()
-        
+
     def content(self, text):
         """Add some content"""
         if self.pretty:
-            self.output += "  "*len(self.stack);
+            self.output += "  " * len(self.stack)
         self.output += str(text)
 
     def save(self, filename):
@@ -63,7 +67,7 @@ for model in models:
         for field in item._meta.fields:
             writer.open(field.name)
             value = getattr(item, field.name)
-            if value != None:
+            if value is not None:
                 if isinstance(value, django.db.models.base.Model):
                     # This field is a foreign key, so save the primary key
                     # of the referring object
