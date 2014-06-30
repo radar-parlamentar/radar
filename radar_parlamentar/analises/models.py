ce63e9096e829d2ba0cd067de56ee1900e38ee90
carol15022 <carol15022@hotmail.com>
2014-06-29 23:53:14 -0300

    Padronização models

--- radar_parlamentar/analises/models.py
+++ radar_parlamentar/analises/models.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2012, Leonardo Leite
 #
 # This file is part of Radar Parlamentar.
-# 
+#
 # Radar Parlamentar is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -13,40 +13,39 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Radar Parlamentar.  If not, see <http://www.gnu.org/licenses/>.
 
+
 class AnaliseTemporal:
-    
+
     def __init__(self):
         self.casa_legislativa = None
         self.periodicidade = None
-        self.analises_periodo = [] 
+        self.analises_periodo = []
         self.votacoes = []
         self.total_votacoes = 0
         self.palavras_chaves = []
 
 
 class AnalisePeriodo:
-    
+
     def __init__(self):
         self.casa_legislativa = None
-        self.periodo = None # PeriodoCasaLegislativa
+        self.periodo = None  # PeriodoCasaLegislativa
         self.partidos = []
         self.votacoes = []
         self.num_votacoes = 0
 
         self.pca = None
 
-        self.tamanhos_partidos = {} # partido => int
-        self.coordenadas_partidos = {} # partdo => [x,y]
+        self.tamanhos_partidos = {}  # partido => int
+        self.coordenadas_partidos = {}  # partdo => [x,y]
         # TODO coordenadas_partidos should be partido.nome => [x,y]
 
-        self.presencas_parlamentares = {} # legislatura.id => boolean        
-        self.coordenadas_legislaturas = {} # legislatura.id => [x,y]
-        self.legislaturas_por_partido = {} # partido.nome => lista das legislaturas do partido (independente de periodo).
-        
-    
-
-    
+        self.presencas_parlamentares = {}  # legislatura.id => boolean
+        self.coordenadas_legislaturas = {}  # legislatura.id => [x,y]
+        self.legislaturas_por_partido = {}
+            # partido.nome => lista das legislaturas do partido (independente
+            # de periodo).
