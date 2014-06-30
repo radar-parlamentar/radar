b2eaa0053e2685025af7c64228825f99fd1e2a35
carol15022 <carol15022@hotmail.com>
2014-06-30 01:12:50 -0300

    Padronização exportador_csv.py

--- radar_parlamentar/exportadores/exportador_csv.py
+++ radar_parlamentar/exportadores/exportador_csv.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+# !/usr/bin/python
 # coding=utf8
 
-# Copyright (C) 2013, Leonardo Leite 
+# Copyright (C) 2013, Leonardo Leite
 #
 # This file is part of Radar Parlamentar.
 #
@@ -28,14 +28,14 @@ import csv
 from modelagem import models
 from django.utils.dateparse import parse_datetime
 import logging
-  logger = logging.getLogger("radar")
+logger = logging.getLogger("radar")
 
 MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 COALITION_PARTIES = ['PT', 'PCdoB', 'PSB', 'PP', 'PMDB', 'PTB']
 # PR, PDT não são coalition?
 
-ROLLCALL = 'rollcall' 
+ROLLCALL = 'rollcall'
 VOTER_ID = 'voter_id'
 NAME = 'name'
 PARTY = 'party'
@@ -46,8 +46,9 @@ LABELS = [ROLLCALL, VOTER_ID, NAME, PARTY, COALITION, VOTE]
 
 CSV_FILE = 'votes.csv'
 
+
 class ExportadorCSV:
-    
+
     def __init__(self, nome_curto_casa_legislativa, data_ini, data_fim):
         self.nome_curto = nome_curto_casa_legislativa
         self.ini = data_ini
@@ -59,18 +60,25 @@ class ExportadorCSV:
         self.retrieve_votacoes()
         self.transform_data()
         self.write_csv()
-            
+
     def retrieve_votacoes(self):
         casa = models.CasaLegislativa.objects.get(nome_curto=self.nome_curto)
-        if self.ini == None and self.fim == None:
-            self.votacoes = models.Votacao.objects.filter(proposicao__casa_legislativa=casa).order_by('data') 
-        if self.ini == None and self.fim != None:
-            self.votacoes = models.Votacao.objects.filter(proposicao__casa_legislativa=casa).filter(data__lte=self.fim).order_by('data')
-        if self.ini != None and self.fim == None:
-            self.votacoes = models.Votacao.objects.filter(proposicao__casa_legislativa=casa).filter(data__gte=self.ini).order_by('data')
-        if self.ini != None and self.fim != None:
-            self.votacoes = models.Votacao.objects.filter(proposicao__casa_legislativa=casa).filter(data__gte=self.ini, data__lte=self.fim).order_by('data')
-    
+        if self.ini is None and self.fim is None:
+            self.votacoes = models.Votacao.objects.filter(
+                proposicao__casa_legislativa=casa).order_by('data')
+        if self.ini is None and self.fim is not None:
+            self.votacoes = models.Votacao.objects.filter(
+                proposicao__casa_legislativa=casa
+            ).filter(data__lte=self.fim).order_by('data')
+        if self.ini is not None and self.fim is None:
+            self.votacoes = models.Votacao.objects.filter(
+                proposicao__casa_legislativa=casa
+            ).filter(data__gte=self.ini).order_by('data')
+        if self.ini is not None and self.fim is not None:
+            self.votacoes = models.Votacao.objects.filter(
+                proposicao__casa_legislativa=casa
+            ).filter(data__gte=self.ini, data__lte=self.fim).order_by('data')
+
     def transform_data(self):
         self.csv_rows.append(LABELS)
         for votacao in self.votacoes:
@@ -91,10 +99,10 @@ class ExportadorCSV:
                 except:
                     print 'Ignorando voto ', voto.opcao
                     logger.info("Ignorando voto: %s" % voto.opcao)
-                
+
     def coalition(self, nome_partido):
         return '1' if nome_partido in COALITION_PARTIES else '0'
-                
+
     def voto(self, opcao):
         if opcao == models.SIM:
             return 1
@@ -106,23 +114,18 @@ class ExportadorCSV:
             return 0
         elif opcao == models.AUSENTE:
             return 0
-        else: 
+        else:
             raise ValueError()
-    
+
     def write_csv(self):
         filepath = os.path.join(MODULE_DIR, 'dados', CSV_FILE)
         with open(filepath, 'wb') as f:
             writer = csv.writer(f, delimiter=',')
             writer.writerows(self.csv_rows)
 
+
 def main():
     data_ini = parse_datetime('2010-06-09 0:0:0')
     data_fim = parse_datetime('2010-06-09 23:59:0')
     exportador = ExportadorCSV('sen', data_ini, data_fim)
     exportador.exportar_csv()
-    
-    
-    
-    
-    
-    
