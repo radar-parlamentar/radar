9de1a6d6d287ee2b64a2b0c5ae5dc001e0e1204a
leonardofl <leonardofl87@gmail.com>
2014-10-01 21:08:38 -0300

    arrumando dump-radar.sh

--- radar_parlamentar/cron/dump-radar.sh
+++ radar_parlamentar/cron/dump-radar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pg_dump -h localhost -U radar radar --inserts -f $RADAR_HOME/static/files/db-dump/radar.sql
+pg_dump -h localhost -U radar radar --inserts -f $RADAR_HOME/static/db-dump/radar.sql
