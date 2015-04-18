85cd6608975d680293e1bda9d77c51cce6067fab
Leonardo Leite <leonardofl87@gmail.com>
2015-04-18 19:29:07 -0300

    retirando verbosidade dos testes

--- radar_parlamentar/unit_tests.sh
+++ radar_parlamentar/unit_tests.sh
@@ -1,1 +1,1 @@
-python manage.py test -v 2 modelagem analises exportadores importadorInterno importadores radar_parlamentar
+python manage.py test modelagem analises exportadores importadorInterno importadores radar_parlamentar
