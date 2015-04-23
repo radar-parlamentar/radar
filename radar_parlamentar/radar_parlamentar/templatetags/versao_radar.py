abc5dd913ea92a3e19072b5e27fa60113c8d0641
Matheus Fonseca <1matheusfonseca@gmail.com>
2015-04-23 12:56:48 +0000

    Adicionando o hash completo do commit no link da versao e colocando o target para _blank

--- radar_parlamentar/radar_parlamentar/templatetags/versao_radar.py
+++ radar_parlamentar/radar_parlamentar/templatetags/versao_radar.py
@@ -16,12 +16,13 @@ def versao_radar():
     try:
         cmd_data_ultimo_commit = 'TZ={0} date -d @$(git log -n1 --format="%at") "+%d/%m/%Y"'.format(time_zone)
         data_ultimo_commit = subprocess.check_output(cmd_data_ultimo_commit, shell=True)
-
-        cmd_hash_ultimo_commit = 'git log --pretty=format:"%h" -n 1'
+        
+        cmd_hash_ultimo_commit = 'git log --pretty=format:"%H" -n 1'
         hash_ultimo_commit = subprocess.check_output(cmd_hash_ultimo_commit, shell=True)
-
-        versao_radar = 'Versão: <a href="https://github.com/radar-parlamentar/radar/commit/{0}">{0}</a> de {1}'.format(hash_ultimo_commit, data_ultimo_commit)
-    except subprocess.CalledProcessError, e:
+        hash_abrev_ultimo_commit = hash_ultimo_commit[:7]
+        
+        versao_radar = 'Versão: <a href="https://github.com/radar-parlamentar/radar/commit/{0}" target="_blank">{1}</a> de {2}'.format(hash_ultimo_commit, hash_abrev_ultimo_commit, data_ultimo_commit)
+    except (IndexError, subprocess.CalledProcessError) as e:
         logger.error('Erro ao pegar o hash ou a data do ultimo commit (versao sera omitida)')
 
     return versao_radar
