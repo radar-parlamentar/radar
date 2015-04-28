4605806bbcf18baa730a498c2c134b097ca2336a
Paulo Tada <paulohtfs@gmail.com>
2015-04-28 16:18:14 +0000

    Finalizando popup de ajuda
    Signed-off-by: Paulo Tada <paulohtfs@gmail.com>
    Signed-off-by: Vanessa Soares <vannessa.andrade@gmail.com>

--- radar_parlamentar/static/files/codes/js/popup_win.js
+++ radar_parlamentar/static/files/codes/js/popup_win.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+	if(checkCookie() == true){
+		setTimeout(openPopup, 2000);
+	}
 	$("#modal-open-button").click(function(e) {
 		openPopup();
 	});
@@ -22,6 +26,7 @@ function openPopup(){
 
 function closePopup(){
 	$("#modal-open-button").prop("disabled", false);
+	$("#modal-open-button").fadeIn();
 	$("#popup-content").fadeOut();
 	$("#overlay-bg").fadeOut();
 }
@@ -34,4 +39,37 @@ function updatePopup(){
 		'top':top,
 		'left':left
 	});
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+	var check = false;
+    var user = getCookie("username");
+    if (user != "") {
+        check = false;
+    } else {
+        user = "user";
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+        check = true;
+    }
+    return check;
 }
