c6336b4bc2cde8beca5583cbd63ccf37329fa610
Leonardo Leite <leonardofl87@gmail.com>
2015-06-09 22:59:05 -0300

    data de dump pro log

--- radar_parlamentar/cron/dump-radar.sh
+++ radar_parlamentar/cron/dump-radar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Argumento: arquivo de saída (onde o dump será salvo)
-echo -e "Gerando dump completo do banco"
+echo "Gerando dump completo do banco em '$(date)'"
 pg_dump -h localhost -U radar radar --inserts -t modelagem_* -f $1
