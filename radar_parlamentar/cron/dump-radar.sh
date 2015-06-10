78978930e91add8499a5b7424a2b034c54fd26a0
Leonardo Leite <leonardofl87@gmail.com>
2015-06-09 22:07:41 -0300

    ajustando geração de sump

--- radar_parlamentar/cron/dump-radar.sh
+++ radar_parlamentar/cron/dump-radar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Argumento: arquivo de saída (onde o dump será salvo)
 echo -e "Gerando dump completo do banco"
-pg_dump -h localhost -U radar radar --inserts -t modelagem_* -f $RADAR_HOME/static/db-dump/radar.sql
+pg_dump -h localhost -U radar radar --inserts -t modelagem_* -f $1
