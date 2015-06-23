23ba5edd9207f52defdd5b628f542f47ba552f83
Leonardo Leite <leonardofl87@gmail.com>
2015-06-23 20:45:41 -0300

    sempre q importa uma casa, passa os dados pro elasticsearch

--- radar_parlamentar/importadores/importador.py
+++ radar_parlamentar/importadores/importador.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from importadores import importador_elasticsearch
+
 def main(lista_casas_legislativas):
 
     for casa_legislativa in lista_casas_legislativas:
@@ -6,18 +8,22 @@ def main(lista_casas_legislativas):
         if casa_legislativa == 'conv':
             from importadores import conv as importador_convencao
             importador_convencao.main()
+            importador_elasticsearch.main()
 
         elif casa_legislativa == 'cmsp':
             from importadores import cmsp as importador_cmsp
             importador_cmsp.main()
+            importador_elasticsearch.main()
 
         elif casa_legislativa == 'sen':
             from importadores import sen as importador_senado
             importador_senado.main()
+            importador_elasticsearch.main()
 
         elif casa_legislativa == 'cdep':
             from importadores import cdep as importador_camara
             importador_camara.main()
+            importador_elasticsearch.main()
 
         else:
             print "Casa %s não encontrada" % casa_legislativa
