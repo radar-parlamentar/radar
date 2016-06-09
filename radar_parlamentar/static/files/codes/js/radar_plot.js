fc1e5cf687435a3ad872df54726538d2e2ba5112
Marco William Silva <marcowilliamdf@gmail.com>
2016-06-09 16:47:18 -0300

    Mostrando chefes do executivo pelo javascript do grafico Signed-off-by: Emilie Morais <emilie.mo@hotmail.com>

--- radar_parlamentar/static/files/codes/js/radar_plot.js
+++ radar_parlamentar/static/files/codes/js/radar_plot.js
@@ -534,7 +534,8 @@ Plot = (function ($) {
                 }
             });            
             
-            label_chefe_executivo.text(periodos[periodo_atual].chefe_executivo);
+            var chefes = get_chefes_executivos();
+            label_chefe_executivo.text(chefes);
             label_periodo.text(periodos[periodo_atual].nome);
             quantidade_votacoes = periodos[periodo_atual].nvotacoes;
             label_nvotacoes.text(quantidade_votacoes + " votações"); 
@@ -545,6 +546,20 @@ Plot = (function ($) {
             if (periodo_para == periodo_min) mouseout_previous();
         }
         
+
+        function get_chefes_executivos(){
+            var chefes = "";
+            chefes_executivos = periodos[periodo_atual].chefe_executivo;
+            for (var i = 0; i < chefes_executivos.length; i++) {
+                if(i != 0){
+                    chefes += " , ";
+                }
+                chefes += chefes_executivos[i]['nome'];
+            }
+
+            return chefes;
+        }
+
         // Issue#272
         function get_partido_parlamentar_pesquisado(){
             partidos_atuais = get_partidos_no_periodo(periodo_atual);
