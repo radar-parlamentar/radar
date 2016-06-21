f16b1eb49a7d2432811b37304cc696e4c6fa0b9c
Emiliemorais <emilie.mo@hotmail.com>
2016-06-21 19:42:56 -0300

    Criando importador para chefes executivos do senado
    Signed-off-by: Luis Henrique <luishng123@gmail.com>
    Signed-off-by: Emiliemorais <emilie.mo@hotmail.com>

--- radar_parlamentar/importadores/chefe_executivo.py
+++
@@ -1,7 +0,0 @@
-import os
-
-MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-XMLCHEFES = os.path.join(MODULE_DIR, 'dados/chefe_executivo/chefe_executivo.xml')
-
-'''Realizar importação do XML para model ChefeExecutivo'''
