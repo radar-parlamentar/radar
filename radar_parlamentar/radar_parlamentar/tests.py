932fac2ad010563a744d6b79db1bf2b0d37665a4
Marco William <marcowilliamdf@gmail.com>
2016-06-28 18:14:57 -0300

    Testando metodo que faz o parser do feed do blog polignu
    Signed-off-by: Joao Araruna <jgararuna@hotmail.com>

--- radar_parlamentar/radar_parlamentar/tests.py
+++ radar_parlamentar/radar_parlamentar/tests.py
@@ -3,7 +3,15 @@
 from django.test import TestCase
 from radar_parlamentar.templatetags.versao_radar import versao_radar
 import re
+from blog import DictionaryBlogGenerator
 
+
+class BlogTest(TestCase):
+
+	def test_cria_dict_blog(self):
+		dict_blog = DictionaryBlogGenerator.cria_dict_blog()
+		blog_title = dict_blog.feed.title
+		self.assertEquals(blog_title,'PoliGNU - Grupo de Estudos de Software Livre da Poli-USP - Radar Parlamentar')
 
 class VersaoRadarTest(TestCase):
 
