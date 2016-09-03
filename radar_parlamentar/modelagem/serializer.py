a281e7e617fbd833a858496fa9404e2017c13d46
AndresMRM <andres@inventati.org>
2016-09-03 17:56:36 -0300

    ordenando JSON proposições

--- radar_parlamentar/modelagem/serializer.py
+++ radar_parlamentar/modelagem/serializer.py
@@ -22,6 +22,8 @@ from django.utils.dateparse import parse_datetime
 from . import models
 import json
 
+from analises.ordenacao import ordem_dos_parlamentares
+
 
 class ProposicaoSerializer():
 
@@ -29,44 +31,67 @@ class ProposicaoSerializer():
         self.partidos_serial = {}
 
     def get_json_proposicao(self, proposicao):
+        '''
+        Recebe um objeto proposição e retorna o JSON da proposição.
+        '''
+
+        # Retorna lista ordenada de votações
+        votacoes = ordem_dos_parlamentares(proposicao)
+
     #    fields = ['id_prop', 'sigla', 'numero', 'ano', 'ementa', 'descricao', 'indexacao', 'data_apresentacao', 'situacao']
         fields = ['id_prop', 'sigla', 'numero', 'ano', 'descricao', 'ementa']
         proposicao_serial = {}
         proposicao_serial['nome'] = proposicao.nome()
         for field in fields:
             proposicao_serial[field] = getattr(proposicao, field)
-        votacoes = models.Votacao.objects.filter(proposicao=proposicao)
         proposicao_serial['votacoes'] = self.get_votacoes_serial(votacoes)
         proposicao_serial['partidos'] = self.partidos_serial
         return json.dumps(proposicao_serial)
 
     def get_votacoes_serial(self, votacoes):
+        '''
+        Recebe votações ordenadas e retorna JSON das votações.
+        '''
         fields = ['id_vot', 'descricao', 'resultado']  # TODO
         votacoes_serial = []
-        for votacao in votacoes:
+        for votacao, parlamentares in votacoes:
             votacao_serial = {}
             votacao_serial['data'] = votacao.data.strftime('%d/%m/%Y')
             for field in fields:
                 votacao_serial[field] = getattr(votacao, field)
-            votos = votacao.votos()
-            votacao_serial['parlamentares'] = \
-                self.get_parlamentares_serial(votos)
+            votacao_serial['parlamentares'] = self.get_parlamentares_serial(
+                parlamentares,
+                self.get_votos_parlamentares(votacao))
             votacoes_serial.append(votacao_serial)
         return votacoes_serial
 
-    def get_parlamentares_serial(self, votos):
+    def get_parlamentares_serial(self, parlamentares, votos):
+        '''
+        Recebe a lista de parlamentares ordenados e um
+        dicionário de votos e retorna o JSON dos parlamentares.
+        '''
         parlamentares_serial = []
-        for voto in votos:
+        for parlamentar in parlamentares:
             parlamentar_serial = {
-               'voto': voto.opcao,
-               'nome': voto.parlamentar.nome,
-               'id_partido': voto.parlamentar.partido.id,
+                'voto': votos[parlamentar.id],
+                'nome': parlamentar.nome,
+                'id_partido': parlamentar.partido.id,
             }
-            self.add_in_partidos_serial(voto.parlamentar.partido)
+            self.add_in_partidos_serial(parlamentar.partido)
             parlamentares_serial.append(parlamentar_serial)
         return parlamentares_serial
 
+    def get_votos_parlamentares(self, votacao):
+        '''
+        Recebe uma votação e retarna um dicionário do voto
+        para cada parlamentar.
+        '''
+        return {voto.parlamentar.id: voto.opcao for voto in votacao.votos()}
+
     def add_in_partidos_serial(self, partido):
+        '''
+        Adiciona um partido ao dicionário de partidos usados.
+        '''
         self.partidos_serial[partido.id] = {
             'nome': partido.nome,
             'numero': partido.numero,
