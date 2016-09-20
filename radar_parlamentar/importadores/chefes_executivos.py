7a7d3af70535eb9133862a00854834c0fb5ceb2a
Eduardo Vital <vitaldu@gmail.com>
2016-09-20 17:45:12 -0300

    Refatorando métodos do arquivo  chefes_executivos - Alterando retorno do método verificar_chefe_existe - Alterando ordem da condição lógica do método salvar_chefe_executivo
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>
    Signed-off-by: Laura Martins <laura.martins.stw@hotmail.com>

--- radar_parlamentar/importadores/chefes_executivos.py
+++ radar_parlamentar/importadores/chefes_executivos.py
@@ -56,19 +56,19 @@ class ImportadorChefesExecutivos:
 
     def salvar_chefe_executivo(self, chefe):
 
-        chefe_existe, chefe_atual = self.verificar_chefe_existe(chefe) #chefe_atual é recebido para adicionarmos relacão com outra casa
+        chefe_atual = self.verificar_chefe_existe(chefe) #chefe_atual é recebido para adicionarmos relacão com outra casa
 
         #Adiciona novo chefe ou adiciona casa a um chefe ja existente ou ignora chefe ja existente de acordo com chefe_existe
-        if (chefe_existe == False):
+        if (chefe_atual):
+            if self.verifica_casa_existe_no_chefe(chefe_atual):
+                logger.warn('Chefe %s já existe' % chefe.nome)
+            else:
+                chefe_atual.casas_legislativas.add(self.casa)
+                logger.info('Adicionando chefe %s em outra casa' % chefe_atual.nome)
+        else:
             chefe.save()
             chefe.casas_legislativas.add(self.casa)
             logger.info('Adicionando chefe %s' % chefe.nome)
-        elif (chefe_existe == True):
-            if not self.verifica_casa_existe_no_chefe(chefe_atual):
-                chefe_atual.casas_legislativas.add(self.casa)
-                logger.info('Adicionando chefe %s em outra casa' % chefe_atual.nome)
-            else:
-                logger.warn('Chefe %s já existe' % chefe.nome)
 
 
     def verificar_chefe_existe(self, chefe):
@@ -82,9 +82,8 @@ class ImportadorChefesExecutivos:
                 ano_fim_igual = chefe_atual.mandato_ano_fim == chefe.mandato_ano_fim
 
                 if (nome_igual and partido_igual and ano_inicio_igual and ano_fim_igual):
-                    return True, chefe_atual
-
-        return False, None #Quando o chefe não existe no banco, não é necessário retornar o chefe_atual
+                    return chefe_atual
+            return None
 
 
     def verifica_casa_existe_no_chefe(self, chefe_atual):
