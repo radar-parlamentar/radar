d0273625f2185118d143fd1189099d693276c8ab
Rafael Akiyoshi <rafah94@hotmail.com>
2016-10-11 19:04:42 -0300

    Criado método de teste para ordenar partidos

--- radar_parlamentar/plenaria/tests/tests_ordenacao.py
+++ radar_parlamentar/plenaria/tests/tests_ordenacao.py
@@ -6,10 +6,29 @@ from modelagem.models import Parlamentar, Proposicao, Votacao, \
 from modelagem import utils
 from modelagem.models import ANO, BIENIO, QUADRIENIO
 from analises.analise import AnalisadorPeriodo
-from datetime import datetime
+from plenaria import ordenacao
+from modelagem import models
+from datetime import date
+from importadores import conv
 from operator import itemgetter
 
-class OrdenacaoTest(TestCase):
+class OrdenacaoTest(TestCase):	
 
-    def test_ordem_dos_parlamentares(self):
-        pass
+	@classmethod
+	def setUpClass(cls):
+		cls.importer = conv.ImportadorConvencao()
+		cls.importer.importar()
+
+	@classmethod
+	def tearDownClass(cls):
+		from util_test import flush_db
+		flush_db(cls)
+
+	def test_ordenar_partidos(self):
+		periodo = models.PeriodoCasaLegislativa(date(1989, 02, 02),
+		date(1989, 10, 10))
+		casa_legislativa = models.CasaLegislativa.objects.get(
+		nome_curto='conv')
+		ordem = []
+		ordem = ordenacao.ordenar_partidos(casa_legislativa, periodo)
+		self.assertEquals(ordem[0].nome, 'Jacobinos')
