9486637cf4fde1d3ba0654f5a1f1c93d14b234b2
MatheusGodinho <matheus.godinho@outlook.com>
2017-04-11 13:03:19 -0300

    Adicionando o teste para o exportador de csv
    Signed-off-by: MatheusGodinho <matheus.godinho@outlook.com>
    Signed-off by: PedroHenriqueSales <pedrohsalesf@gmail.com>
    x

--- radar_parlamentar/exportadores/tests/tests_exportacao.py
+++ radar_parlamentar/exportadores/tests/tests_exportacao.py
@@ -2,20 +2,30 @@ from django.test import TestCase
 from exportadores import exportador_csv
 from modelagem.models import CasaLegislativa
 from modelagem.models import Votacao
+import os.path
+import csv
 
-class ExportacaoClass(TestCase):	
+class ExportacaoClass(TestCase):
 
+	@classmethod
+	def setUpClass(cls):
+		exportador_csv.main()
 
-	# # @classmethod
-	# # def setUpClass(cls):
-	# # 	cls.importer = conv.ImportadorConvencao()
-	# # 	cls.importer.importar()
-
-	# @classmethod
-	# def tearDownClass(cls):
-	# 	from util_test import flush_db
-	# 	flush_db(cls)
+	#@classmethod
+	#def tearDownClass(cls):
+	#	from util_test import flush_db
+	#	flush_db(cls)
 
 
 	def test_exportar_cvs(self):
-		exportador_csv.main()
+            CAMINHO = "./exportadores/saida/votacoes.csv"
+            colunas_csv = []
+            existe = False
+	    existe = os.path.isfile(CAMINHO)
+	    colunas = ["PROPOSICAO","VOTACAO","PARLAMENTAR_ID","PARLAMENTAR_NOME","PARTIDO","UF","REGIAO","VOTO"]
+            with open(CAMINHO,'rb') as arquivo:
+                readers = csv.reader(arquivo, delimiter=',')
+                for row in readers:
+                    colunas_csv = row
+	    self.assertEquals(True, existe)
+            self.assertEquals(colunas_csv, colunas)
