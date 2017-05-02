22ed06c3127411a8936e14ddd9023ee67c428b70
Jessica Suzuki <suzuki_jessica@hotmail.com>
2017-05-02 20:51:12 -0300

    adicionando testes em importadores

--- radar_parlamentar/importadores/tests/tests_camara_genero.py
+++ radar_parlamentar/importadores/tests/tests_camara_genero.py
@@ -4,18 +4,29 @@ from importadores.camara_genero import multiple_null_remove, proposicoes_indexad
 
 class CamaraTest(TestCase):
 
-    def test__null_to_none(self):
+    def test__null_to_none_com_null(self):
         proposicao = {"Casa":"NULL"}
         self.assertEquals(_null_to_none(proposicao), {"Casa":None})
 
-    def test_multiple_null_remove(self):
-        lista_proposicoes = [{"Casa":1}, {"Educacao":2}, {"Transporte":"NULL"}]
-        self.assertEquals(multiple_null_remove(lista_proposicoes), [{"Casa":1}, {"Educacao":2}, {"Transporte":None}])
+    def test__null_to_none_sem_null(self):
+        proposicao = {"Casa":1}
+        self.assertEquals(_null_to_none(proposicao), {"Casa":1})
 
-    def test_proposicoes_indexadas(self):
-    	indexados = [{'txtIndexacao':1, 'txtSiglaPartido': "AL"}]
-    	lista_proposicoes = [{'txtIndexacao':1, 'txtSiglaPartido': "AL"}]
-    	self.assertEquals(proposicoes_indexadas(lista_proposicoes), indexados)
+    def test_multiple_null_remove(self):
+        lista_proposicoes_null = [{"Casa":1}, {"Educacao":"NULL"}, {"Transporte":"NULL"}]
+        lista_proposicoes = [{"Casa:":1, "Lar":2}]
+        self.assertEquals(multiple_null_remove(lista_proposicoes_null), [{"Casa":1}, {"Educacao":None}, {"Transporte":None}])
+        self.assertEquals(multiple_null_remove(lista_proposicoes), [{"Casa:":1, "Lar":2}])
+
+    def test_proposicoes_indexadas_partido_lista(self):
+    	indexados_partido = [{'txtIndexacao':1, 'txtSiglaPartido': "AL"}]
+    	lista_proposicoes_partido = [{'txtIndexacao':1, 'txtSiglaPartido': "AL"}]
+    	self.assertEquals(proposicoes_indexadas(lista_proposicoes_partido), indexados_partido)
+
+    def test_proposicoes_indexadas_partido_errado(self):
+        indexados = []
+        lista_proposicoes = [{'txtIndexacao':1, 'txtSiglaPartido': "ALLL"}]
+        self.assertEquals(proposicoes_indexadas(lista_proposicoes), indexados)
 
     def test_parseia_indexacoes(self):
         indexacao = "Teste\nRadar.Parlamentar, _radar_parlamentar_"
