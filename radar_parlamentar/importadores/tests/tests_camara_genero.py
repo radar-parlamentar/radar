30855ea9be4323ac1c159e8a4419340e78f26950
Jessica Suzuki <suzuki_jessica@hotmail.com>
2017-05-02 17:05:37 -0300

    adicionando testes em tests_camara_genero.py
    Signed-off-by: suzukijessica <suzuki_jessica@hotmail.com>
    Signed-off-by: pedrohenriquesales <pedrohsalesf@gmail.com>

--- radar_parlamentar/importadores/tests/tests_camara_genero.py
+++ radar_parlamentar/importadores/tests/tests_camara_genero.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from importadores.camara_genero import _null_to_none
+from importadores.camara_genero import multiple_null_remove
 
 class CamaraTest(TestCase):
 
     def test__null_to_none(self):
-        proposicao = {"CASA":"NULL"}
-        self.assertEquals(_null_to_none(proposicao), {"CASA": None})
+        proposicao = {"Casa":"NULL"}
+        self.assertEquals(_null_to_none(proposicao), {"Casa":None})
+
+    def test__multiple_null_remove(self):
+        lista_proposicoes = [{"Casa":1}, {"Educacao":2}, {"Transporte":"NULL"}]
+        self.assertEquals(multiple_null_remove(lista_proposicoes), [{"Casa":1}, {"Educacao":2}, {"Transporte":None}])
