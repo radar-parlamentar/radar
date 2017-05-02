cb00430e2790d869e7895999951a4db035d5f347
Jessica Suzuki <suzuki_jessica@hotmail.com>
2017-05-02 18:27:52 -0300

    adicionando teste em tests_camara_genero.py do modulo importadores
    Signed-off-by: suzukijessica <suzuki_jessica@hotmail.com>
    Signed-off-by: pedrohenriquesales <pedrohsalesf@gmail.com>

--- radar_parlamentar/importadores/tests/tests_camara_genero.py
+++ radar_parlamentar/importadores/tests/tests_camara_genero.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from importadores.camara_genero import _null_to_none
-from importadores.camara_genero import multiple_null_remove, proposicoes_indexadas
+from importadores.camara_genero import multiple_null_remove, proposicoes_indexadas, parseia_indexacoes
 
 class CamaraTest(TestCase):
 
@@ -16,3 +16,7 @@ class CamaraTest(TestCase):
     	indexados = [{'txtIndexacao':1, 'txtSiglaPartido': "AL"}]
     	lista_proposicoes = [{'txtIndexacao':1, 'txtSiglaPartido': "AL"}]
     	self.assertEquals(proposicoes_indexadas(lista_proposicoes), indexados)
+
+    def test_parseia_indexacoes(self):
+        indexacao = "Teste\nRadar.Parlamentar, _radar_parlamentar_"
+        self.assertEquals(parseia_indexacoes(indexacao), [u'testeradarparlamentar', u'radarparlamentar'])
