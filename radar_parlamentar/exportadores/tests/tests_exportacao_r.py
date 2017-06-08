44ed2d56c65a543cf7529cdbc42a34e2684f7cf5
luisresende <luisfrvilela@gmail.com>
2017-06-08 15:53:45 -0300

    mudando identacao dos metodos

--- radar_parlamentar/exportadores/tests/tests_exportacao_r.py
+++ radar_parlamentar/exportadores/tests/tests_exportacao_r.py
@@ -16,16 +16,15 @@ class ExportacaoRClass(TestCase):
 	#	from util_test import flush_db
 	#	flush_db(cls)
 
-
 	def test_exportar_cvs_r(self):
-            CAMINHO = "./exportadores/saida/votes.csv"
-            colunas_csv = []
-            existe = False
-	    existe = os.path.isfile(CAMINHO)
-	    colunas = ["rollcall","voter_id","name","party","coalition","vote"]
-            with open(CAMINHO,'rb') as arquivo:
-                readers = csv.reader(arquivo, delimiter=',')
-                for row in readers:
-                    colunas_csv = row
-	    self.assertEqual(True, existe)
-            self.assertEqual(colunas_csv, colunas)
+		CAMINHO = "./exportadores/saida/votes.csv"
+		colunas_csv = []
+		existe = False
+		existe = os.path.isfile(CAMINHO)
+		colunas = ["rollcall","voter_id","name","party","coalition","vote"]
+		with open(CAMINHO,'rb') as arquivo:
+			readers = csv.reader(arquivo, delimiter=',')
+			for row in readers:
+				colunas_csv = row
+		self.assertEqual(True, existe)
+		self.assertEqual(colunas_csv, colunas)
