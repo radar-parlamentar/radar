438859a289eb09cbf86d0735819f4a898abb5f8f
Jessica Suzuki <suzuki_jessica@hotmail.com>
2017-06-22 18:13:36 -0300

    Modificando exportadores/tests/tests_exportacao_r.py de acordo com pep8

--- radar_parlamentar/exportadores/tests/tests_exportacao_r.py
+++ radar_parlamentar/exportadores/tests/tests_exportacao_r.py
@@ -5,26 +5,28 @@ from modelagem.models import Votacao
 import os.path
 import csv
 
+
 class ExportacaoRClass(TestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		exportador_csv_r.main()
+    @classmethod
+    def setUpClass(cls):
+        exportador_csv_r.main()
 
-	#@classmethod
-	#def tearDownClass(cls):
-	#	from util_test import flush_db
-	#	flush_db(cls)
+    # @classmethod
+    # def tearDownClass(cls):
+    # from util_test import flush_db
+    # flush_db(cls)
 
-	def test_exportar_cvs_r(self):
-		CAMINHO = "./exportadores/saida/votes.csv"
-		colunas_csv = []
-		existe = False
-		existe = os.path.isfile(CAMINHO)
-		colunas = ["rollcall","voter_id","name","party","coalition","vote"]
-		with open(CAMINHO,'r') as arquivo:
-			readers = csv.reader(arquivo, delimiter=',')
-			for row in readers:
-				colunas_csv = row
-		self.assertEqual(True, existe)
-		self.assertEqual(colunas_csv, colunas)
+    def test_exportar_cvs_r(self):
+        CAMINHO = "./exportadores/saida/votes.csv"
+        colunas_csv = []
+        existe = False
+        existe = os.path.isfile(CAMINHO)
+        colunas = ["rollcall", "voter_id", "name", "party", "coalition",
+                                                            "vote"]
+        with open(CAMINHO, 'r') as arquivo:
+            readers = csv.reader(arquivo, delimiter=',')
+            for row in readers:
+                colunas_csv = row
+        self.assertEqual(True, existe)
+        self.assertEqual(colunas_csv, colunas)
