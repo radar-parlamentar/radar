5037c1e3c59c0f2056e89ea8521b0230a889056d
Jessica Suzuki <suzuki_jessica@hotmail.com>
2017-06-23 13:29:34 -0300

    Modificando modulo plenaria de acordo com pep8

--- radar_parlamentar/plenaria/views.py
+++ radar_parlamentar/plenaria/views.py
@@ -8,17 +8,19 @@ from modelagem import utils
 from . import serializer
 
 
-def plenaria(request, nome_curto_casa_legislativa=None, identificador_proposicao=None):
+def plenaria(request,
+             nome_curto_casa_legislativa=None,
+             identificador_proposicao=None):
     """identificador_proposicao: 'PL-123-2015'"""
 
     casas_legislativas = []
     for casa in models.CasaLegislativa.objects.all():
-        #TODO: Remover após resolução da issue #377
+        # TODO: Remover após resolução da issue #377
         #       ( https://github.com/radar-parlamentar/radar/issues/377 )
         if casa.nome_curto != "cdep":
             casas_legislativas.append((casa.nome_curto, casa.nome))
 
-    #TODO: Aqui precisa de uma verificação para quando não houver nenhuma casa.
+    # TODO: Aqui precisa de uma verificação para quando não houver nenhuma casa
 
     proposicoes = []
     if nome_curto_casa_legislativa is None:
@@ -53,17 +55,22 @@ def identificador(prop):
     return '-'.join([prop.sigla, prop.numero, prop.ano])
 
 
-def json_proposicao(request, nome_curto_casa_legislativa, identificador_proposicao=None):
+def json_proposicao(request, nome_curto_casa_legislativa,
+                    identificador_proposicao=None):
     """Retorna o JSON com os dados de todas as votações de uma proposição.
         identificador_proposicao: 'PL-123-2015'
     """
-    sigla_proposicao, numero_proposicao, ano_proposicao = identificador_proposicao.split('-')
+    sigla_proposicao, numero_proposicao, ano_proposicao = \
+        identificador_proposicao.split('-')
 # comentado enquanto não se resolve #388
-#    proposicao = get_object_or_404(
-#        models.Proposicao, casa_legislativa__nome_curto=nome_curto_casa_legislativa,
-#        sigla=sigla_proposicao, numero=numero_proposicao, ano=ano_proposicao)
-    proposicoes = models.Proposicao.objects.filter(casa_legislativa__nome_curto=nome_curto_casa_legislativa,
-        sigla=sigla_proposicao, numero=numero_proposicao, ano=ano_proposicao)
+# proposicao = get_object_or_404(
+# models.Proposicao, casa_legislativa__nome_curto=nome_curto_casa_legislativa,
+# sigla=sigla_proposicao, numero=numero_proposicao, ano=ano_proposicao)
+    proposicoes = \
+        models.Proposicao.objects.filter(
+            casa_legislativa__nome_curto=nome_curto_casa_legislativa,
+            sigla=sigla_proposicao, numero=numero_proposicao,
+            ano=ano_proposicao)
     if proposicoes:
         proposicao = proposicoes[0]
     else:
@@ -71,4 +78,3 @@ def json_proposicao(request, nome_curto_casa_legislativa, identificador_proposic
     proposicao_serializer = serializer.ProposicaoSerializer()
     dados = proposicao_serializer.get_json_proposicao(proposicao)
     return HttpResponse(dados, mimetype='application/json')
-
