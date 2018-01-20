e765dae3a7221e55da1e84ab73692bde916f3be1
Diego Rabatone Oliveira <diego.rabatone@easytaxi.com.br>
2018-01-20 18:09:37 -0200

    Inicia refatoração do urls.py
    Signed-off-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>
    Signed-off-by: leonardofl <leonardofl87@gmail.com>
    
    Co-authored-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>
    Co-authored-by: leonardofl <leonardofl87@gmail.com>

--- radar_parlamentar/radar_parlamentar/urls.py
+++ radar_parlamentar/radar_parlamentar/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from django.conf.urls.i18n import i18n_patterns
 from django.shortcuts import redirect
 
 from . import views as radar_views
@@ -32,24 +31,8 @@ url_lista_legada = 'lista_de_votacoes_filtradas/'
 url_json_analise_legada = 'json_analise/'
 url_json_plenaria_legada = 'json_plenaria/'
 
-urlpatterns = i18n_patterns(
-    # Change the admin prefix here to use an alternate URL for the
-    # admin interface, which would be marginally more secure.
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # path('admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns = [
     path('admin/', admin.site.urls),
-)
-
-urlpatterns += [
-    # Examples:
-    # path('',
-    #     radar_views.home, name='home'),
-    # path('radar_parlamentar/', include(
-    # 'radar_parlamentar.foo.urls')),
-
-    # Index
     path('', radar_views.index, name="index"),
     path('index/', redirect, {'url': '/'}),
     path('origem/', radar_views.origem, name="origem"),
@@ -57,11 +40,15 @@ urlpatterns += [
     path('premiacoes/', radar_views.premiacoes, name="premiacoes"),
     path('radarnamidia/', radar_views.radar_na_midia, name="radar_na_midia"),
     path('sim-voto-aberto/', radar_views.votoaberto, name="votoaberto"),
+    path('blog/', radar_views.generate_blog_news, name="blog"),
+    path('dados/downloads/', radar_views.dados_utilizados),
+    path('dados', radar_views.dados_utilizados),  # URL legada
+]
+
+urlpatterns += [
     path('dados/importadores/', radar_views.importadores, name="importadores"),
     path('importar/<nome_curto_casa_legislativa>/',
          importadores_views.importar),
-    path('dados/downloads/', radar_views.dados_utilizados),
-    path('blog/', radar_views.generate_blog_news, name="blog"),
     # Páginas do Projeto Gênero do Hackathon da Câmara dos Deputados em 2013
     path('genero/', radar_views.genero, name="genero"),
     path('genero/tematica/partido/', radar_views.genero_matriz,
