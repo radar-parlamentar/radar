811013b3af1ce11598082537b0aed1dd2966fe38
Diego Rabatone Oliveira <diego.rabatone@easytaxi.com.br>
2018-01-20 16:09:12 -0200

    Corrige plenaria.tests.tests_ordenacao
    Signed-off-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>
    Signed-off-by: leonardofl <leonardofl87@gmail.com>
    
    Co-authored-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>
    Co-authored-by: leonardofl <leonardofl87@gmail.com>

--- radar_parlamentar/plenaria/tests/tests_ordenacao.py
+++ radar_parlamentar/plenaria/tests/tests_ordenacao.py
@@ -26,7 +26,7 @@ class OrdenacaoTest(TestCase):
         flush_db(cls)
 
     def test_ordem_dos_parlamentares(self):
-        proposicao = models.Proposicao.objects.get(id=3)
+        proposicao = models.Proposicao.objects.all()[2]
         resultado = ordenacao.ordem_dos_parlamentares(proposicao)
         self.assertEqual(resultado[0][0].descricao,
                          'Institui o Dia de Carlos Magno')
@@ -44,31 +44,8 @@ class OrdenacaoTest(TestCase):
         self.assertEqual(ordem[2].nome, 'Monarquistas')
 
     def test_def_ordenar_votantes(self):
-        proposicao = models.Proposicao.objects.get(id=1)
+        proposicao = models.Proposicao.objects.first()
         resultado = ordenacao.ordenar_votantes(proposicao)
-        parla = Parlamentar.objects.filter(nome='Pierre')[0]
-        primeiro = (parla.partido, 1)
-        self.assertEqual(resultado[parla], primeiro)
-
-        # AttributeError: 'QuerySet' object has no attribute 'voto_set',
-        # QuerySet não está reconhecendo o _set
-
-    """def test_ordem_dos_parl_por_votacao(self):
-        proposicao = models.Proposicao.objects.get(id=1)
-        votacao =
-            Votacao.objects.filter(proposicao=proposicao).order_by('data')
-        casa_legislativa = proposicao.casa_legislativa
-        datas_votacoes = [votac.data for votac in votacao]
-        pr = utils.PeriodosRetriever(casa_legislativa = casa_legislativa,
-            periodicidade = QUADRIENIO,
-            data_da_primeira_votacao = min(datas_votacoes),
-            data_da_ultima_votacao = max(datas_votacoes),
-            numero_minimo_de_votacoes = 1 )
-        periodo = pr.get_periodos()[-1]
-        lista_ordenada_partidos =
-            ordenacao.ordenar_partidos(casa_legislativa, periodo)
-        dicionario_votantes = ordenacao.ordenar_votantes(proposicao)
-        partidos = ordenacao.ordenar_partidos(casa_legislativa, periodo)
-        teste =
-            ordenacao.ordem_dos_parl_por_votacao(votacao,dicionario_votantes,partidos)
-        print teste"""
+        parlamentar = Parlamentar.objects.filter(nome='Pierre')[0]
+        primeiro = (parlamentar.partido, 1)
+        self.assertEqual(resultado[parlamentar], primeiro)
