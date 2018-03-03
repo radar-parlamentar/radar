3e0308121da7cf2df10434aa2e9797e20c31e122
leonardofl <leonardofl87@gmail.com>
2018-03-03 17:38:48 -0300

    Remove ConsoleExceptionMiddleware.
    Com o uso do Docker, não olharemos mais a saída padrão, e sim o log em /var/logs/radar/radar.log.
    E as exceções já estão indo para esse arquivo de log, graças ao ExceptionLoggingMiddleware.
    Com isso, o ConsoleExceptionMiddleware já não é mais necessário.
    
    Signed-off-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>
    Signed-off-by: leonardofl <leonardofl87@gmail.com>
    
    Co-authored-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>
    Co-authored-by: leonardofl <leonardofl87@gmail.com>

--- radar_parlamentar/radar_parlamentar/middleware.py
+++ radar_parlamentar/radar_parlamentar/middleware.py
@@ -17,15 +17,3 @@ class ExceptionLoggingMiddleware(object):
     def process_exception(self, request, exception):
         logger.exception('Exception handling request for ' + request.path)
         return None
-
-class ConsoleExceptionMiddleware:
-    """Custom middleware to print stack traces on Django console"""
-
-    def process_exception(self, request, exception):
-        import traceback
-        import sys
-        exc_info = sys.exc_info()
-        exception = traceback.format_exception(*(exc_info or sys.exc_info()))
-        print("######################## Exception ###########################")
-        print('\n'.join(exception))
-        print("##############################################################")
