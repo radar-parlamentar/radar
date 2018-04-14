3cbc98084860097810024d8bb0dc2d81415d7e15
leonardofl <leonardofl87@gmail.com>
2018-04-14 11:26:06 -0300

    Evolui CashRefresherJob.

--- radar_parlamentar/cron/jobs.py
+++ radar_parlamentar/cron/jobs.py
@@ -3,6 +3,8 @@ import logging
 from importadores.celery_tasks import importar_assincronamente
 from datetime import date
 from django.core.cache import cache
+import requests
+from modelagem import models
 
 logger = logging.getLogger("radar")
 
@@ -51,7 +53,20 @@ class CashRefresherJob(CronJobBase):
     def do(self):
         logger.info('CashRefresherJob foi chamado.')
         cache.clear()
-        # chama as análises
+        casas = [ casa.nome_curto for casa in models.CasaLegislativa.objects.all() ]
+        periodicidades = [ periodo[0] for periodo in  models.PERIODOS ]
+        if 'MES' in periodicidades:
+            periodicidades.remove('MES')
+        for casa in casas:
+            for periodicidade in periodicidades:
+                url = 'http://nginx/radar/json/%s/%s/' % (casa, periodicidade)
+                # localhost não é alcançável, pois o django é acessado via socket
+                logger.info('CashRefresherJob invocando', url)
+                requests.get(url)
+                # Retorno:
+                # <h1>Bad Request (400)</h1>
+                # Obs: Do host, 'GET http://localhost/radar/json/cmsp/ANO/'
+                # funciona.
 
-# TODO class CashRefresherJob - Executado diariamente às 1h
+
 # TODO class DbDumperJob - Executado segunda às 4h
